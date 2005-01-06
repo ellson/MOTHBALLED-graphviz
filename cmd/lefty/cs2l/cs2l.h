2aac35932cc04522cccd0faa384831b4dee7c038
ellson <devnull@localhost>
2005-01-06 15:01:43 +0000

    add lefty, dotty, lneato to graphviz2 tree

---
+++ cmd/lefty/cs2l/cs2l.h
@@ -0,0 +1,29 @@
+/* $Id$ $Revision$ */
+/* vim:set shiftwidth=4 ts=8: */
+
+/**********************************************************
+*      This software is part of the graphviz package      *
+*                http://www.graphviz.org/                 *
+*                                                         *
+*            Copyright (c) 1994-2004 AT&T Corp.           *
+*                and is licensed under the                *
+*            Common Public License, Version 1.0           *
+*                      by AT&T Corp.                      *
+*                                                         *
+*        Information and Software Systems Research        *
+*              AT&T Research, Florham Park NJ             *
+**********************************************************/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifndef _C2L_H
+#define _C2L_H
+    int C2Lopen(char *, char *, FILE **, FILE **);
+#endif				/* _C2L_H */
+
+#ifdef __cplusplus
+}
+#endif
