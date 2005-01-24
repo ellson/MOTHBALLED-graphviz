6d57eb03a91add3463c2ed7b26537106180f7608
ellson <devnull@localhost>
2005-01-24 18:44:45 +0000

    remove old license text from top of file (CPL text is OK)

--- lib/common/psgen.c
+++ lib/common/psgen.c
@@ -15,16 +15,6 @@
 **********************************************************/
 
 /*
-    This software may only be used by you under license from AT&T Corp.
-    ("AT&T").  A copy of AT&T's Source Code Agreement is available at
-    AT&T's Internet website having the URL:
-    <http://www.research.att.com/sw/tools/graphviz/license/source.html>
-    If you received this software without first entering into a license
-    with AT&T, you have an infringing copy of this software and cannot use
-    it without violating AT&T's intellectual property rights.
-*/
-
-/*
     J$: added `pdfmark' URL embedding.  PostScript rendered from
         dot files with URL attributes will get active PDF links
         from Adobe's Distiller.
