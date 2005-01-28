3ef7da60259906e2935ee98c224f97abbc2949b5
ellson <devnull@localhost>
2005-01-28 20:46:29 +0000

    add pie, same as fillpie

--- tclpkg/gdtclft/examples/arc.tcl
+++ tclpkg/gdtclft/examples/arc.tcl
@@ -16,7 +16,8 @@ gd chord     $gd $black 100 160 120 40 225 315
 gd fillchord $gd $black 100 200 120 40 225 315
 gd openchord $gd $black 100 240 120 40 225 315
 
-gd fillpie   $gd $black 100 280 120 40 225 315
-gd openpie   $gd $black 100 320 120 40 225 315
+gd pie       $gd $black 100 280 120 40 225 315
+gd fillpie   $gd $black 100 320 120 40 225 315
+gd openpie   $gd $black 100 360 120 40 225 315
 
 gd writePNG $gd stdout
