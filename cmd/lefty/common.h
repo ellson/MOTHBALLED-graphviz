ffb61531244d91310f56790f77126dcb2571e5a7
erg <devnull@localhost>
2005-04-08 20:45:33 +0000

    Update with new lefty, fixing many bugs and supporting new features

--- cmd/lefty/common.h
+++ cmd/lefty/common.h
@@ -18,69 +18,24 @@
 extern "C" {
 #endif
 
-
-/* Lefteris Koutsofios - AT&T Bell Laboratories */
+/* Lefteris Koutsofios - AT&T Labs Research */
 
 #ifndef _COMMON_H
 #define _COMMON_H
 
 /* some config and conversion definitions from graphviz distribution */
-#ifdef HAVE_CONFIG_H
 #include "config.h"
-#endif
-#ifdef MSWIN32
-#define FEATURE_WIN32
-#define FEATURE_MS
-#else
-#ifndef FEATURE_GTK
-#define FEATURE_X11
-#endif
-#endif
-#ifdef HAVECS
-#define FEATURE_CS
-#endif
-#ifdef HAVENETSCAPE
-#define FEATURE_NETSCAPE
-#endif
-#ifdef HAVEGMAP
-#define FEATURE_GMAP
-#define FEATURE_MINTSIZE
-#endif
-#ifdef HAVEDOT
-#define FEATURE_DOT
-#endif
-#ifdef GNU
-#define FEATURE_GNU
-#endif
-#ifdef HAVERUSAGE
-#define FEATURE_RUSAGE
-#endif
-/* */
 
-#ifdef FEATURE_CS
+#ifdef _PACKAGE_ast
 #include <ast.h>
+#define HAVE_STRERROR 1
 #else
-#ifdef HAVE_STDLIB_H
 #include <stdlib.h>
-#endif
-#ifdef HAVE_STDARG_H
 #include <stdarg.h>
-#endif
-#ifdef HAVE_STRING_H
 #include <string.h>
-#endif
-#ifdef HAVE_LIMITS_H
 #include <limits.h>
-#endif
-#ifdef HAVE_SYS_TYPES_H
 #include <sys/types.h>
-#endif
-#ifdef HAVE_SYS_SELECT_H
-#include <sys/select.h>
-#endif
-#ifdef HAVE_SYS_STAT_H
 #include <sys/stat.h>
-#endif
 #endif
 
 #include <math.h>
@@ -92,6 +47,7 @@ extern "C" {
 #include <windows.h>
 #include <commdlg.h>
 #endif
+
 #ifdef FEATURE_MS
 #include <malloc.h>
 #endif
@@ -115,31 +71,22 @@ extern "C" {
 #define M_PI 3.14159265358979323846
 #endif
 
-#ifndef REALSTRCMP
-#define Strcmp(s1, s2) ( \
-    *(s1) == *(s2) ? ( \
-        (*s1) ? strcmp ((s1) + 1, (s2) + 1) : 0 \
-    ) : (*(s1) < *(s2) ? -1 : 1) \
-)
-#else
-#define Strcmp(s1, s2) strcmp ((s1), (s2))
-#endif
+extern int warnflag;
+extern char *leftypath, *leftyoptions, *shellpath;
+extern jmp_buf exitljbuf;
+extern int idlerunmode;
+extern fd_set inputfds;
 
-    extern int warnflag;
-    extern char *leftypath, *leftyoptions, *shellpath;
-    extern jmp_buf exitljbuf;
-    extern int idlerunmode;
-    extern fd_set inputfds;
-
-    int init(char *);
-    void term(void);
-    char *buildpath(char *, int);
-    char *buildcommand(char *, char *, int, int, char *);
-    void warning(char *, int, char *, char *, ...);
-    void panic(char *, int, char *, char *, ...);
-    void panic2(char *, int, char *, char *, ...);
-#endif				/* _COMMON_H */
+int init (char *);
+void term (void);
+char *buildpath (char *, int);
+char *buildcommand (char *, char *, int, int, char *);
+void warning (char *, int, char *, char *, ...);
+void panic (char *, int, char *, char *, ...);
+void panic2 (char *, int, char *, char *, ...);
+#endif /* _COMMON_H */
 
 #ifdef __cplusplus
 }
 #endif
+
--- cmd/lefty/display.c
+++ cmd/lefty/display.c
@@ -14,8 +14,7 @@
 *              AT&T Research, Florham Park NJ             *
 **********************************************************/
 
-
-/* Lefteris Koutsofios - AT&T Bell Laboratories */
+/* Lefteris Koutsofios - AT&T Labs Research */
 
 #include "common.h"
 #include "mem.h"
@@ -25,7 +24,7 @@
 #include "display.h"
 
 typedef struct dnode_t {
-    Ttype_t ttype;
+    int ttype;
     Tobj ko, vo;
     char *path;
 } dnode_t;
@@ -41,48 +40,44 @@ static int seeni, seenn;
 
 static int indent, afternl;
 
-static void update(dnode_t *);
-static int cmp(const void *, const void *);
-static seennode_t *findseen(dnode_t *);
-static void add2seen(dnode_t *);
-static void pr(char *);
+static void update (dnode_t *);
+static int cmp (const void *, const void *);
+static seennode_t *findseen (dnode_t *);
+static void add2seen (dnode_t *);
+static void pr (char *);
 
-void Dinit(void)
-{
-    seenp = Marrayalloc((long) SEENINCR * SEENSIZE);
+void Dinit (void) {
+    seenp = Marrayalloc ((long) SEENINCR * SEENSIZE);
     seeni = 0;
     seenn = SEENINCR;
 }
 
-void Dterm(void)
-{
-    Marrayfree(seenp);
+void Dterm (void) {
+    Marrayfree (seenp);
     seeni = seenn = 0;
 }
 
-void Dtrace(Tobj to, int offset)
-{
+void Dtrace (Tobj to, int offset) {
     dnode_t dnode;
     char *s;
     int i;
 
     indent = offset - 2;
     afternl = TRUE;
-    if (Tgettype(to) != T_TABLE) {
-	pr((s = Ssfull(NULL, to))), free(s);
-	return;
+    if (Tgettype (to) != T_TABLE) {
+        pr ((s = Ssfull (NULL, to))), free (s);
+        return;
     }
 
     seeni = 0;
     dnode.vo = to;
     dnode.path = "";
-    update(&dnode);
+    update (&dnode);
     for (i = 0; i < seeni; i++)
-	free(seenp[i].path), seenp[i].path = NULL;
+        free (seenp[i].path), seenp[i].path = NULL;
 }
 
-static void update(dnode_t * pnode)
-{
+static void update (dnode_t *pnode) {
     Tkvindex_t tkvi;
     dnode_t *list, *cnode;
     seennode_t *seennode;
@@ -91,94 +86,93 @@ static void update(dnode_t * pnode)
 
     indent += 2;
     n = ((Ttable_t *) pnode->vo)->n;
-    if (!(list = malloc(n * sizeof(dnode_t))))
-	panic(POS, "update", "list malloc failed");
-    for (cnode = &list[0], Tgetfirst(pnode->vo, &tkvi); tkvi.kvp;
-	 cnode++, Tgetnext(&tkvi)) {
-	cnode->ko = tkvi.kvp->ko;
-	cnode->vo = tkvi.kvp->vo;
-	cnode->ttype = Tgettype(cnode->vo);
+    if (!(list = malloc (n * sizeof (dnode_t))))
+        panic (POS, "update", "list malloc failed");
+    for (
+        cnode = &list[0], Tgetfirst (pnode->vo, &tkvi); tkvi.kvp;
+        cnode++, Tgetnext (&tkvi)
+    ) {
+        cnode->ko = tkvi.kvp->ko;
+        cnode->vo = tkvi.kvp->vo;
+        cnode->ttype = Tgettype (cnode->vo);
     }
-    qsort((char *) list, n, sizeof(dnode_t), cmp);
+    qsort ((char *) list, n, sizeof (dnode_t), cmp);
     for (i = 0, cnode = &list[0]; i < n; i++, cnode++) {
-	cnode->path = Spath(pnode->path, cnode->ko);
-	seennode = findseen(cnode);
-	if (seennode) {
-	    pr((s = Sseen(cnode->ko, seennode->path))), free(s);
-	} else {
-	    add2seen(cnode);
-	    if (cnode->ttype == T_TABLE) {
-		pr((s = Stfull(cnode->ko))), free(s);
-		update(cnode);
-		pr("];");
-	    } else {
-		pr((s = Ssfull(cnode->ko, cnode->vo))), free(s);
-	    }
-	}
+        cnode->path = Spath (pnode->path, cnode->ko);
+        seennode = findseen (cnode);
+        if (seennode) {
+            pr ((s = Sseen (cnode->ko, seennode->path))), free (s);
+        } else {
+            add2seen (cnode);
+            if (cnode->ttype == T_TABLE) {
+                pr ((s = Stfull (cnode->ko))), free (s);
+                update (cnode);
+                pr ("];");
+            } else {
+                pr ((s = Ssfull (cnode->ko, cnode->vo))), free (s);
+            }
+        }
     }
-    free(list);
+    free (list);
     indent -= 2;
 }
 
-static int cmp(const void *a, const void *b)
-{
-    Ttype_t atype, btype;
+static int cmp (const void *a, const void *b) {
+    int atype, btype;
     dnode_t *anode, *bnode;
-    double d1 = 0.0, d2 = 0.0;
+    double d1, d2;
 
+    d1 = 0.0, d2 = 0.0;
     anode = (dnode_t *) a, bnode = (dnode_t *) b;
-    atype = Tgettype(anode->ko), btype = Tgettype(bnode->ko);
+    atype = Tgettype (anode->ko), btype = Tgettype (bnode->ko);
     if (atype != btype)
-	return (atype - btype);
+        return (atype - btype);
     if (atype == T_STRING)
-	return Strcmp(Tgetstring(anode->ko), Tgetstring(bnode->ko));
+        return strcmp (Tgetstring (anode->ko), Tgetstring (bnode->ko));
     if (atype == T_INTEGER)
-	d1 = Tgetinteger(anode->ko), d2 = Tgetinteger(bnode->ko);
+        d1 = Tgetinteger (anode->ko), d2 = Tgetinteger (bnode->ko);
     else if (atype == T_REAL)
-	d1 = Tgetreal(anode->ko), d2 = Tgetreal(bnode->ko);
+        d1 = Tgetreal (anode->ko), d2 = Tgetreal (bnode->ko);
     if (d1 < d2)
-	return -1;
+        return -1;
     else if (d1 > d2)
-	return 1;
+        return 1;
     else
-	return 0;		/* but this should never happen since keys are unique */
+        return 0; /* but this should never happen since keys are unique */
 }
 
-static seennode_t *findseen(dnode_t * cnode)
-{
+static seennode_t *findseen (dnode_t *cnode) {
     int i;
 
     for (i = 0; i < seeni; i++)
-	if (seenp[i].vo == cnode->vo)
-	    return &seenp[i];
+        if (seenp[i].vo == cnode->vo)
+            return &seenp[i];
     return NULL;
 }
 
-static void add2seen(dnode_t * cnode)
-{
+static void add2seen (dnode_t *cnode) {
     if (seeni >= seenn) {
-	seenp = Marraygrow(seenp, (long) (seenn + SEENINCR) * SEENSIZE);
-	seenn += SEENINCR;
+        seenp = Marraygrow (seenp, (long) (seenn + SEENINCR) * SEENSIZE);
+        seenn += SEENINCR;
     }
     seenp[seeni].vo = cnode->vo;
     seenp[seeni++].path = cnode->path;
 }
 
-static void pr(char *s)
-{
+static void pr (char *s) {
     char *s1;
     int i;
 
     for (s1 = s; *s1; s1++) {
-	if (afternl) {
-	    for (i = 0; i < indent; i++)
-		putchar(' ');
-	    afternl = FALSE;
-	}
-	if (*s1 == '\n')
-	    afternl = TRUE;
-	putchar((*s1));		/* HACK: to keep proto happy */
+        if (afternl) {
+            for (i = 0; i < indent; i++)
+                putchar (' ');
+            afternl = FALSE;
+        }
+        if (*s1 == '\n')
+            afternl = TRUE;
+        putchar ((*s1)); /* HACK: to keep proto happy */
     }
-    putchar('\n');
+    putchar ('\n');
     afternl = TRUE;
 }
