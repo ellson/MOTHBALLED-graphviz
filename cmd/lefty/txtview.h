d4654bbf9f2f710e662e41826f365b294a845fad
erg <devnull@localhost>
2005-04-08 20:45:34 +0000

    Update with new lefty, fixing many bugs and supporting new features

--- cmd/lefty/txtview.h
+++ cmd/lefty/txtview.h
@@ -18,19 +18,18 @@
 extern "C" {
 #endif
 
-
-/* Lefteris Koutsofios - AT&T Bell Laboratories */
+/* Lefteris Koutsofios - AT&T Labs Research */
 
 #ifndef _TXTVIEW_H
 #define _TXTVIEW_H
-    void TXTinit(Grect_t);
-    void TXTterm(void);
-    int TXTmode(int argc, lvar_t * argv);
-    int TXTask(int argc, lvar_t * argv);
-    void TXTprocess(int, char *);
-    void TXTupdate(void);
-    void TXTtoggle(int, void *);
-#endif				/* _TXTVIEW_H */
+void TXTinit (Grect_t);
+void TXTterm (void);
+int TXTmode (int argc, lvar_t *argv);
+int TXTask (int argc, lvar_t *argv);
+void TXTprocess (int, char *);
+void TXTupdate (void);
+void TXTtoggle (int, void *);
+#endif /* _TXTVIEW_H */
 
 #ifdef __cplusplus
 }
