e5410c25e5287699260ba3d3ecd188faaf30e499
erg <devnull@localhost>
2005-04-08 20:45:33 +0000

    Update with new lefty, fixing many bugs and supporting new features

--- cmd/lefty/display.h
+++ cmd/lefty/display.h
@@ -13,21 +13,20 @@
 *        Information and Software Systems Research        *
 *              AT&T Research, Florham Park NJ             *
 **********************************************************/
-
+    
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-
-/* Lefteris Koutsofios - AT&T Bell Laboratories */
+/* Lefteris Koutsofios - AT&T Labs Research */
 
 #ifndef _DISPLAY_H
 #define _DISPLAY_H
-    void Dinit(void);
-    void Dterm(void);
-    void Dtrace(Tobj, int);
-#endif				/* _DISPLAY_H */
+void Dinit (void);
+void Dterm (void);
+void Dtrace (Tobj, int);
+#endif /* _DISPLAY_H */
 
 #ifdef __cplusplus
-}
+}   
 #endif
