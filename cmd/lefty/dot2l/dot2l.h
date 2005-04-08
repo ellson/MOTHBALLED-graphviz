07a281526219bb94b451252b40776088e568519c
erg <devnull@localhost>
2005-04-08 20:45:34 +0000

    Update with new lefty, fixing many bugs and supporting new features

--- cmd/lefty/dot2l/dot2l.h
+++ cmd/lefty/dot2l/dot2l.h
@@ -23,54 +23,55 @@ extern "C" {
 #define _DOT2L_H
 #define SMALLBUF 128
 
-    int yyparse(void);
-    int yylex(void);
+int yyparse(void);
+int yylex(void);
 
-    typedef struct edgeframe_t {
-	struct edgeframe_t *next;
-	int type;
-	void *obj;
-	char *port;
-    } edgeframe_t;
+typedef struct edgeframe_t {
+    struct edgeframe_t *next;
+    int type;
+    void *obj;
+    char *port;
+} edgeframe_t;
 
-    typedef struct graphframe_t {
-	struct graphframe_t *next;
-	Tobj g, graphs, nodes, edges;
-	Tobj gattr, nattr, eattr, ecopy;
-	long emark;
-	struct edgeframe_t *estack;
-    } graphframe_t;
+typedef struct graphframe_t {
+    struct graphframe_t *next;
+    Tobj g, graphs, nodes, edges;
+    Tobj gattr, nattr, eattr, ecopy;
+    long emark;
+    struct edgeframe_t *estack;
+} graphframe_t;
 
-    typedef enum {
-	NODE, EDGE, GRAPH
-    } objtype_t;
+typedef enum {
+    NODE, EDGE, GRAPH
+} objtype_t;
 
-    extern char *gtype, *etype;
-    extern int yaccdone;
-    extern int attrclass;
-    extern int inattrstmt;
+extern char *gtype, *etype;
+extern int yaccdone;
+extern int attrclass;
+extern int inattrstmt;
 
 #ifndef TRUE
 #define FALSE 0
 #define TRUE 1
 #endif
 
-    Tobj D2Lparsegraphlabel(Tobj, Tobj);
-    Tobj D2Lreadgraph(int, Tobj);
-    void D2Lwritegraph(int, Tobj, int);
-    void D2Lbegin(char *);
-    void D2Lend(void);
-    void D2Labort(void);
-    void D2Lpushgraph(char *);
-    Tobj D2Lpopgraph(void);
-    Tobj D2Linsertnode(char *);
-    void D2Linsertedge(Tobj, char *, Tobj, char *);
-    void D2Lbeginedge(int, Tobj, char *);
-    void D2Lmidedge(int, Tobj, char *);
-    void D2Lendedge(void);
-    void D2Lsetattr(char *, char *);
-#endif				/* _DOT2L_H */
+Tobj D2Lparsegraphlabel (Tobj, Tobj);
+Tobj D2Lreadgraph (int, Tobj);
+void D2Lwritegraph (int, Tobj, int);
+void D2Lbegin (char *);
+void D2Lend (void);
+void D2Labort (void);
+void D2Lpushgraph (char *);
+Tobj D2Lpopgraph (void);
+Tobj D2Linsertnode (char *);
+void D2Linsertedge (Tobj, char *, Tobj, char *);
+void D2Lbeginedge (int, Tobj, char *);
+void D2Lmidedge (int, Tobj, char *);
+void D2Lendedge (void);
+void D2Lsetattr (char *, char *);
+#endif /* _DOT2L_H */
 
 #ifdef __cplusplus
 }
 #endif
+
