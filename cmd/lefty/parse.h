d382aa8fca9778c0251ff9c8e708731c2a1f07ab
erg <devnull@localhost>
2005-04-08 20:45:34 +0000

    Update with new lefty, fixing many bugs and supporting new features

--- cmd/lefty/parse.h
+++ cmd/lefty/parse.h
@@ -18,25 +18,24 @@
 extern "C" {
 #endif
 
-
-/* Lefteris Koutsofios - AT&T Bell Laboratories */
+/* Lefteris Koutsofios - AT&T Labs Research */
 
 #ifndef _PARSE_H
 #define _PARSE_H
-    typedef struct Psrc_t {
-	int flag;
-	char *s;
-	FILE *fp;
-	int tok;
-	int lnum;
-    } Psrc_t;
+typedef struct Psrc_t {
+    int flag;
+    char *s;
+    FILE *fp;
+    int tok;
+    int lnum;
+} Psrc_t;
 
-    void Pinit(void);
-    void Pterm(void);
-    Tobj Punit(Psrc_t *);
-    Tobj Pfcall(Tobj, Tobj);
-    Tobj Pfunction(char *, int);
-#endif				/* _PARSE_H */
+void Pinit (void);
+void Pterm (void);
+Tobj Punit (Psrc_t *);
+Tobj Pfcall (Tobj, Tobj);
+Tobj Pfunction (char *, int);
+#endif /* _PARSE_H */
 
 #ifdef __cplusplus
 }
