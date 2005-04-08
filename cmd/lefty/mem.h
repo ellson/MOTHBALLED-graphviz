a29cbc321b647c4fb286de788ad744aa35f5842d
erg <devnull@localhost>
2005-04-08 20:45:34 +0000

    Update with new lefty, fixing many bugs and supporting new features

--- cmd/lefty/mem.h
+++ cmd/lefty/mem.h
@@ -13,64 +13,62 @@
 *        Information and Software Systems Research        *
 *              AT&T Research, Florham Park NJ             *
 **********************************************************/
-
+            
 #ifdef __cplusplus
 extern "C" {
-#endif
+#endif  
 
-
-/* Lefteris Koutsofios - AT&T Bell Laboratories */
+/* Lefteris Koutsofios - AT&T Labs Research */
 
 #ifndef _MEM_H
 #define _MEM_H
 
 #ifdef FEATURE_MINTSIZE
-    typedef int Msize_t;
+typedef int Msize_t;
 #define M_SIZEMAX INT_MAX
 #else
-    typedef short Msize_t;
+typedef short Msize_t;
 #define M_SIZEMAX SHRT_MAX
 #endif
-    typedef struct Mheader_t {
-	char type;
-	char area;
-	Msize_t size;
-    } Mheader_t;
+typedef struct Mheader_t {
+    char type;
+    char area;
+    Msize_t size;
+} Mheader_t;
 #define M_HEADERSIZE sizeof (Mheader_t)
 
-    typedef enum {
-	M_GCOFF, M_GCON
-    } Mgcstate_t;
+#define M_GCOFF 0
+#define M_GCON  1
 
-    typedef enum {
-	M_GCFULL, M_GCINCR
-    } Mgctype_t;
+#define M_GCFULL 0
+#define M_GCINCR 1
 
 #define M_UNITSIZE sizeof (long)
 #define M_BYTE2SIZE(l) ((long) (((l + M_UNITSIZE - 1) / M_UNITSIZE)))
 #define M_AREAOF(p) ((int) (((Mheader_t *) p)->area))
 #define M_TYPEOF(p) ((int) (((Mheader_t *) p)->type))
 
-    extern int Mhaspointers[];
-    extern Mgcstate_t Mgcstate;
-    extern int Mcouldgc;
+extern int Mhaspointers[];
+extern int Mgcstate;
+extern int Mcouldgc;
 
-    void Minit(void (*)(void));
-    void Mterm(void);
-    void *Mnew(long, int);
-    void *Mallocate(long);
-    void Mfree(void *, long);
-    void *Marrayalloc(long);
-    void *Marraygrow(void *, long);
-    void Marrayfree(void *);
-    long Mpushmark(void *);
-    void Mpopmark(long);
-    void Mresetmark(long, void *);
-    void Mmkcurr(void *);
-    void Mdogc(Mgctype_t);
-    void Mreport(void);
-#endif				/* _MEM_H */
+void Minit (void (*) (void));
+void Mterm (void);
+void *Mnew (long, int);
+void *Mallocate (long);
+void Mfree (void *, long);
+void *Marrayalloc (long);
+void *Marraygrow (void *, long);
+void Marrayfree (void *);
+long Mpushmark (void *);
+void Mpopmark (long);
+void Mresetmark (long, void *);
+void Mmkcurr (void *);
+void Mdogc (int);
+void Mreport (void);
+#endif /* _MEM_H */
 
 #ifdef __cplusplus
 }
 #endif
+
