a4a5d761f9cee11c229b9a10189505ead3324bc5
erg <devnull@localhost>
2005-04-08 20:45:34 +0000

    Update with new lefty, fixing many bugs and supporting new features

--- cmd/lefty/str.h
+++ cmd/lefty/str.h
@@ -18,20 +18,19 @@
 extern "C" {
 #endif
 
-
-/* Lefteris Koutsofios - AT&T Bell Laboratories */
+/* Lefteris Koutsofios - AT&T Labs Research */
 
 #ifndef _STR_H
 #define _STR_H
-    void Sinit(void);
-    void Sterm(void);
-    char *Spath(char *, Tobj);
-    char *Sseen(Tobj, char *);
-    char *Sabstract(Tobj, Tobj);
-    char *Stfull(Tobj);
-    char *Ssfull(Tobj, Tobj);
-    char *Scfull(Tobj, int, int);
-#endif				/* _STR_H */
+void Sinit (void);
+void Sterm (void);
+char *Spath (char *, Tobj);
+char *Sseen (Tobj, char *);
+char *Sabstract (Tobj, Tobj);
+char *Stfull (Tobj);
+char *Ssfull (Tobj, Tobj);
+char *Scfull (Tobj, int, int);
+#endif /* _STR_H */
 
 #ifdef __cplusplus
 }
