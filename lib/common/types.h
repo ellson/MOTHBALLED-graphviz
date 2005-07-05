45f08a78a44545975e7bcb332dda6e56cd9efd84
ellson <devnull@localhost>
2005-07-05 19:21:46 +0000

    adding infrastructure for a usershape plugin

--- lib/common/types.h
+++ lib/common/types.h
@@ -39,6 +39,7 @@ extern "C" {
     typedef struct gvrender_engine_s gvrender_engine_t;
     typedef struct gvlayout_engine_s gvlayout_engine_t;
     typedef struct gvtextlayout_engine_s gvtextlayout_engine_t;
+    typedef struct gvusershape_engine_s gvusershape_engine_t;
 
     typedef struct htmllabel_t htmllabel_t;
 
---
+++ lib/gvc/gvplugin_usershape.h
@@ -0,0 +1,36 @@
+/* $Id$ $Revision$ */
+/* vim:set shiftwidth=4 ts=8: */
+
+/**********************************************************
+*      This software is part of the graphviz package      *
+*                http://www.graphviz.org/                 *
+*                                                         *
+*            Copyright (c) 1994-2004 AT&T Corp.           *
+*                and is licensed under the                *
+*            Common Public License, Version 1.0           *
+*                      by AT&T Corp.                      *
+*                                                         *
+*        Information and Software Systems Research        *
+*              AT&T Research, Florham Park NJ             *
+**********************************************************/
+
+#ifndef GVUSERSHAPE_PLUGIN_H
+#define GVUSERSHAPE_PLUGIN_H
+
+#include "gvplugin.h"
+#include "gvcint.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    struct gvusershape_engine_s {
+	void *(*getshape) (char *name);
+	void (*freeshapes) (void);
+	point (*image_size) (graph_t * g, char *shapefile);
+    };
+
+#ifdef __cplusplus
+}
+#endif
+#endif				/* GVUSERSHAPE_PLUGIN_H */
