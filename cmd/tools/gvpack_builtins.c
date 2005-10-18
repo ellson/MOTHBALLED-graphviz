66c7fcb7de38aee3127867fae456ca2852ec1bbe
ellson <devnull@localhost>
2005-10-18 18:30:07 +0000

    add missing files

---
+++ cmd/tools/gvpack_builtins.c
@@ -0,0 +1,25 @@
+/* $Id$ $Revision$ */
+/* vim:set shiftwidth=4 ts=8: */
+
+/**********************************************************
+*      This software is part of the graphviz package      *
+*                http://www.graphviz.org/                 *
+*                                                         *
+*            Copyright (c) 1994-2004 AT&T Corp.           *
+*                and is licensed under the                *
+*            Common Public License, Version 1.0           *
+*                      by AT&T Corp.                      *
+*                                                         *
+*        Information and Software Systems Research        *
+*              AT&T Research, Florham Park NJ             *
+**********************************************************/
+
+#include "gvplugin.h"
+
+extern gvplugin_library_t gvplugin_neato_layout_LTX_library;
+
+const lt_symlist_t lt_preloaded_symbols[] = {
+    { "gvplugin_neato_layout_LTX_library", (void*)(&gvplugin_neato_layout_LTX_library) },
+    { 0, 0 }
+};
+
