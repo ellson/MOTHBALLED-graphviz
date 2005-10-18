0e497f3af40d164e53f33a6210c36ef0bccbf23c
ellson <devnull@localhost>
2005-10-18 18:53:17 +0000

    cut back on overloading: set -> getv + setv

--- tclpkg/gv/test.pl
+++ tclpkg/gv/test.pl
@@ -3,17 +3,17 @@
 use gv;
 
 $g = gv::digraph "G";
-print gv::set($g, "aaa", "xxx"), "\n";
-print gv::set($g, "aaa"), "\n";
+print gv::setv($g, "aaa", "xxx"), "\n";
+print gv::getv($g, "aaa"), "\n";
 $n = gv::node $g, "hello";
-print gv::set($n,"label"), "\n";
-print gv::set($n, "aaa", "xxx"), "\n";
-print gv::set($n, "aaa"), "\n";
+print gv::getv($n,"label"), "\n";
+print gv::setv($n, "aaa", "xxx"), "\n";
+print gv::getv($n, "aaa"), "\n";
 $m = gv::node $g, "world";
-print gv::set($m, "aaa"), "\n";
+print gv::getv($m, "aaa"), "\n";
 $e = gv::edge $n, $m;
-print gv::set($e, "aaa", "xxx"), "\n";
-print gv::set($e, "aaa"), "\n";
+print gv::setv($e, "aaa", "xxx"), "\n";
+print gv::getv($e, "aaa"), "\n";
 gv::rm $e;
 gv::rm $n;
 gv::rm $m;
