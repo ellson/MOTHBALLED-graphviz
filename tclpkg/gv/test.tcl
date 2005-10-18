f68abb75fe7b1c518c9fb4901708cb4b42d623e3
ellson <devnull@localhost>
2005-10-18 18:53:17 +0000

    cut back on overloading: set -> getv + setv

--- tclpkg/gv/test.tcl
+++ tclpkg/gv/test.tcl
@@ -3,18 +3,18 @@
 load libgv_tcl.so gv
 
 set g [gv::digraph G]
-puts [gv::set $g aaa xxx]
-puts [gv::set $g aaa]
+puts [gv::setv $g aaa xxx]
+puts [gv::getv $g aaa]
 set sg [gv::graph $g SG]
 set n [gv::node $g hello]
-puts [gv::set $n label]
-puts [gv::set $n aaa xxx]
-puts [gv::set $n aaa]
+puts [gv::getv $n label]
+puts [gv::setv $n aaa xxx]
+puts [gv::getv $n aaa]
 set m [gv::node $g world]
-puts [gv::set $m aaa]
+puts [gv::getv $m aaa]
 set e [gv::edge $n $m]
-puts [gv::set $e aaa xxx]
-puts [gv::set $e aaa]
+puts [gv::setv $e aaa xxx]
+puts [gv::getv $e aaa]
 gv::rm $e
 gv::rm $n
 gv::rm $m
@@ -27,14 +27,14 @@ set g [gv::read hello.dot]
 for {set n [gv::firstnode $g]} {[gv::ok $n]} {set n [gv::nextnode $g $n]} {
     puts [gv::nameof $n]
     for {set a [gv::firstattr $n]} {[gv::ok $a]} {set a [gv::nextattr $n $a]} {
-        puts "    [gv::nameof $a] [gv::set $n $a]"
+        puts "    [gv::nameof $a] [gv::getv $n $a]"
     }
 }
 for {set n [gv::firstnode $g]} {[gv::ok $n]} {set n [gv::nextnode $g $n]} {
     for {set e [gv::firstout $n]} {[gv::ok $e]} {set e [gv::nextout $n $e]} {
 	puts "[gv::nameof $n] -> [gv::nameof [gv::headof $e]]"
 	for {set a [gv::firstattr $e]} {[gv::ok $a]} {set a [gv::nextattr $e $a]} {
-	    puts "    [gv::nameof $a] [gv::set $e $a]"
+	    puts "    [gv::nameof $a] [gv::getv $e $a]"
 	}
     }
 }
