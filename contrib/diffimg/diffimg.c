66953954df20c49d1fb5246c29b514ed77d97ee8
ellson <devnull@localhost>
2005-10-18 18:53:33 +0000

    add .ps to diffimg by using system(gs ...) for the conversion

--- contrib/diffimg/diffimg.c
+++ contrib/diffimg/diffimg.c
@@ -29,9 +29,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <gd.h>
 
 #define ABS(x) (((x) < 0) ? -(x) : (x))
+
+static char *pstopng="gs -dNOPAUSE -sDEVICE=pngalpha -sOutputFile=- -q -";
+
+static gdImagePtr imageLoad (char *filename)
+{
+    FILE *f;
+    char *ext, *cmd, *tmp, *base;
+    gdImagePtr im;
+    int rc;
+    struct stat statbuf;
+
+    ext = strrchr(filename, '.');
+    if (!ext) {
+        fprintf(stderr, "Filename \"%s\" has no file extension.\n", filename);
+        exit(1);
+    }
+    rc = stat(filename, &statbuf);
+    if (rc) {
+	 fprintf(stderr, "Failed to stat \"%s\"\n", filename);
+         exit(1);
+    }
+    if (strcasecmp(ext, ".ps") == 0) {
+	ext = ".png";
+	base = strrchr(filename, '/');
+	if (base != filename)
+	    base++;
+	tmp = malloc(strlen(base) + 20 + strlen(ext));
+	strcpy(tmp,"/tmp/diffimg-");
+	strcat(tmp,base);
+	strcat(tmp,ext);
+	
+	cmd = malloc(strlen(pstopng) + strlen(filename) + strlen(tmp) + 10);
+	strcpy(cmd,pstopng);
+	strcat(cmd," <");
+	strcat(cmd,filename);
+	strcat(cmd," >");
+	strcat(cmd,tmp);
+	rc = system(cmd);
+	
+        f = fopen(tmp, "rb");
+	free(cmd);
+	free(tmp);
+    }
+    else
+        f = fopen(filename, "rb");
+    if (!f) {
+        fprintf(stderr, "Failed to open \"%s\"\n", filename);
+        exit(1);
+    }
+    im = 0;
+    if (strcasecmp(ext, ".png") == 0) 
+        im = gdImageCreateFromPng(f);
+    else if (strcasecmp(ext, ".gif") == 0)
+        im = gdImageCreateFromGif(f);
+    else if (strcasecmp(ext, ".jpg") == 0)
+        im = gdImageCreateFromJpeg(f);
+    fclose(f);
+    if (!im) {
+        fprintf(stderr, "Loading image from file  \"%s\" failed!\n", filename);
+        exit(1);
+    }
+    return im;
+}
 
 static void imageDiff (gdImagePtr dst, gdImagePtr src,
 			int dstX, int dstY,
@@ -55,37 +121,6 @@ static void imageDiff (gdImagePtr dst, gdImagePtr src,
             gdImageSetPixel (dst, dstX + x, dstY + y, d);
         }
     }
-}
-
-static gdImagePtr imageLoad (char *filename)
-{
-    FILE *f;
-    char *ext;
-    gdImagePtr im;
-
-    f = fopen(filename, "rb");
-    if (!f) {
-        fprintf(stderr, "Input file \"%s\" does not exist!\n", filename);
-        exit(1);
-    }
-    ext = strrchr(filename, '.');
-    if (!ext) {
-        fprintf(stderr, "Filename \"%s\" has no file extension.\n", filename);
-        exit(1);
-    }
-    im = 0;
-    if (strcasecmp(ext, ".png") == 0) 
-        im = gdImageCreateFromPng(f);
-    else if (strcasecmp(ext, ".gif") == 0)
-        im = gdImageCreateFromGif(f);
-    else if (strcasecmp(ext, ".jpg") == 0)
-        im = gdImageCreateFromJpeg(f);
-    fclose(f);
-    if (!im) {
-        fprintf(stderr, "Loading image from file  \"%s\" failed!\n", filename);
-        exit(1);
-    }
-    return im;
 }
 
 int main(int argc, char **argv)
