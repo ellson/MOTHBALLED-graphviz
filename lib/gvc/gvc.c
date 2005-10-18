3ca1d201f697168161e14be223e17d8a3b1f4e34
ellson <devnull@localhost>
2005-10-18 19:12:11 +0000

    missed change

--- lib/gvc/gvc.c
+++ lib/gvc/gvc.c
@@ -13,9 +13,6 @@
 *        Information and Software Systems Research        *
 *              AT&T Research, Florham Park NJ             *
 **********************************************************/
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
