0ea09e105c4efd188df2dba97380135a10d24d4d
ellson <devnull@localhost>
2006-01-27 20:26:25 +0000

    show edge positions as well

--- tclpkg/gv/examples/layout.py
+++ tclpkg/gv/examples/layout.py
@@ -7,10 +7,10 @@ sys.path.append('/usr/lib/graphviz/python')
 import gv
 
 # create a new empty graph 
-G = gv.digraph("G")
+G = gv.digraph('G')
 
 # define a simple graph ( A->B )
-gv.edge(gv.node(G, "A"),gv.node(G, "B"))
+gv.edge(gv.node(G, 'A'),gv.node(G, 'B'))
 
 # compute a directed graph layout
 gv.layout(G, 'dot')
@@ -21,5 +21,9 @@ gv.render(G)
 # do something with the layout
 n = gv.firstnode(G)
 while gv.ok(n) :
-    print "node " + gv.nameof(n) + " is at " + gv.getv(n,"pos")
+    print 'node '+gv.nameof(n)+' is at '+gv.getv(n,'pos')
+    e = gv.firstout(n)
+    while gv.ok(e) :
+	print 'edge '+gv.nameof(gv.tailof(e))+'->'+gv.nameof(gv.headof(e))+' is at '+gv.getv(e,'pos')
+	e = gv.nextout(n,e)
     n = gv.nextnode(G,n)
