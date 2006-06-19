3007451e4e1ae3d1520992fa8c6f31a6c4d9351e
ellson <devnull@localhost>
2006-06-19 14:37:35 +0000

    no need for entry points if the libraries are empty stubs

--- lib/gvc/dot_builtins.c
+++ lib/gvc/dot_builtins.c
@@ -22,15 +22,23 @@
 
 extern gvplugin_library_t *gvplugin_dot_layout_LTX_library;
 extern gvplugin_library_t *gvplugin_neato_layout_LTX_library;
+#ifdef HAVE_LIBGD
 extern gvplugin_library_t *gvplugin_gd_LTX_library;
+#endif
+#ifdef HAVE_PANGOCAIRO
 extern gvplugin_library_t *gvplugin_pango_LTX_library;
+#endif
 extern gvplugin_library_t *gvplugin_core_LTX_library;
 
 const lt_symlist_t lt_preloaded_symbols[] = {
 	{ "gvplugin_dot_layout_LTX_library", (void*)(&gvplugin_dot_layout_LTX_library) },
 	{ "gvplugin_neato_layout_LTX_library", (void*)(&gvplugin_neato_layout_LTX_library) },
+#ifdef HAVE_PANGOCAIRO
 	{ "gvplugin_pango_LTX_library", (void*)(&gvplugin_pango_LTX_library) },
+#endif
+#ifdef HAVE_LIBGD
 	{ "gvplugin_gd_LTX_library", (void*)(&gvplugin_gd_LTX_library) },
+#endif
 	{ "gvplugin_core_LTX_library", (void*)(&gvplugin_core_LTX_library) },
 	{ 0, 0 }
 };
