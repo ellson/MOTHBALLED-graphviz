bf4453cfec3239060339457187e8cd4dca34033d
ellson <devnull@localhost>
2006-07-08 17:33:49 +0000

    remove DMALLOC bits various include fixups (eg some missing string.h)

--- lib/agraph/attr.c
+++ lib/agraph/attr.c
@@ -15,11 +15,7 @@
 **********************************************************/
 
 
-#include	<aghdr.h>
-
-#ifdef DMALLOC
-#include "dmalloc.h"
-#endif
+#include "aghdr.h"
 
 /*
  * dynamic attributes
