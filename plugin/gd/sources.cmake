ff4e72c9fd571a32b977cd235a023fcfc25af515
ellson <devnull@localhost>
2006-07-08 23:36:09 +0000

    cmake stuff

---
+++ plugin/gd/sources.cmake
@@ -0,0 +1,18 @@
+# $Id$ $Revision$
+
+INCLUDE_DIRECTORIES(
+	${gvplugin_gd_SRCDIR}
+	${top_SRCDIR}
+	${common_SRCDIR}
+	${graph_SRCDIR}
+	${cdt_SRCDIR}
+	${pathplan_SRCDIR}
+	${gvc_SRCDIR}
+)
+
+SET(gvplugin_gd_SRCS
+	${gvplugin_gd_SRCDIR}/gvplugin_gd.c
+	${gvplugin_gd_SRCDIR}/gvrender_gd.c
+	${gvplugin_gd_SRCDIR}/gvtextlayout_gd.c
+	${gvplugin_gd_SRCDIR}/gvloadimage_gd.c
+)
