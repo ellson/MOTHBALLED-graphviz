b33542f77c240b07e58a4e4b1d56748c4292c708
ellson <devnull@localhost>
2007-08-21 20:12:25 +0000

    add new "formatter" plugin api update plugin api version number add new formatter for cairo using gdk_pixbuf  - working add new formatter for cairo using DevIL - partially working add new formatter for cairo using gd - not working

---
+++ plugin/devil/gvplugin_devil.c
@@ -0,0 +1,26 @@
+/* $Id$ $Revision$ */
+/* vim:set shiftwidth=4 ts=8: */
+
+/**********************************************************
+*      This software is part of the graphviz package      *
+*                http://www.graphviz.org/                 *
+*                                                         *
+*            Copyright (c) 1994-2004 AT&T Corp.           *
+*                and is licensed under the                *
+*            Common Public License, Version 1.0           *
+*                      by AT&T Corp.                      *
+*                                                         *
+*        Information and Software Systems Research        *
+*              AT&T Research, Florham Park NJ             *
+**********************************************************/
+
+#include "gvplugin.h"
+
+extern gvplugin_installed_t gvformatter_devil_types;
+
+static gvplugin_api_t apis[] = {
+    {API_formatter, &gvformatter_devil_types},
+    {(api_t)0, 0},
+};
+
+gvplugin_library_t gvplugin_devil_LTX_library = { "devil", apis };
