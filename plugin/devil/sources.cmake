b627417903ebe75c9e651ec99c7d5bcb5ed17c64
ellson <devnull@localhost>
2007-08-21 20:12:25 +0000

    add new "formatter" plugin api update plugin api version number add new formatter for cairo using gdk_pixbuf  - working add new formatter for cairo using DevIL - partially working add new formatter for cairo using gd - not working

---
+++ plugin/devil/sources.cmake
@@ -0,0 +1,22 @@
+# $Id$ $Revision$
+
+INCLUDE( UsePkgConfig )
+PKGCONFIG( pangocairo PANGOCAIRO_INCLUDE_DIR PANGOCAIRO_LIB_DIR PANGOCAIRO_LINK_FLAGS PANGOCAIRO_CFLAGS )
+ADD_DEFINITIONS(${PANGOCAIRO_CFLAGS})
+PKGCONFIG( devil DEVIL_INCLUDE_DIR DEVIL_LIB_DIR DEVIL_LINK_FLAGS DEVIL_CFLAGS )
+ADD_DEFINITIONS(${DEVIL_CFLAGS})
+
+INCLUDE_DIRECTORIES(
+	${gvplugin_devil_SRCDIR}
+	${top_SRCDIR}
+	${common_SRCDIR}
+	${graph_SRCDIR}
+	${cdt_SRCDIR}
+	${pathplan_SRCDIR}
+	${gvc_SRCDIR}
+)
+
+SET(gvplugin_devil_SRCS
+	${gvplugin_devil_SRCDIR}/gvplugin_devil.c
+	${gvplugin_devil_SRCDIR}/gvrender_devil.c
+)
