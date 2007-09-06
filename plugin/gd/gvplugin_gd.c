d2feeb14092f331e0ccb8b4ff8c48156afa07ef0
ellson <devnull@localhost>
2007-09-06 17:32:55 +0000

    shorten some core and gd renderer names

--- plugin/gd/gvplugin_gd.c
+++ plugin/gd/gvplugin_gd.c
@@ -17,7 +17,7 @@
 #include "gvplugin.h"
 
 extern gvplugin_installed_t gvrender_gd_types;
-extern gvplugin_installed_t gvrender_gd_vrml_types;
+extern gvplugin_installed_t gvrender_vrml_types;
 extern gvplugin_installed_t gvtextlayout_gd_types;
 extern gvplugin_installed_t gvloadimage_gd_types;
 extern gvplugin_installed_t gvdevice_gd_types;
@@ -26,7 +26,7 @@ extern gvplugin_installed_t gvdevice_vrml_types;
 
 static gvplugin_api_t apis[] = {
     {API_render, &gvrender_gd_types},
-    {API_render, &gvrender_gd_vrml_types},
+    {API_render, &gvrender_vrml_types},
     {API_textlayout, &gvtextlayout_gd_types},
     {API_loadimage, &gvloadimage_gd_types},
     {API_device, &gvdevice_gd_types},
