6adebd571b58c111ce42b69c580535c6a67ac758
ellson <devnull@localhost>
2007-11-18 03:13:12 +0000

    only use external write discipline for writes to stdout, writes to named now work normally

--- lib/gvc/gvdevice.c
+++ lib/gvc/gvdevice.c
@@ -79,7 +79,7 @@ void gvdevice_printf(GVJ_t * job, const char *format, ...)
 
 size_t gvdevice_write (GVJ_t * job, const unsigned char *s, unsigned int len)
 {
-    if (job->gvc->write_fn)   /* externally provided write dicipline */
+    if (job->gvc->write_fn && job->output_file == stdout)   /* externally provided write dicipline */
 	return (job->gvc->write_fn)(s, len);
     if (job->flags & GVDEVICE_COMPRESSED_FORMAT) {
 #ifdef HAVE_LIBZ
