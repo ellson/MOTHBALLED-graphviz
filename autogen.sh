b9a51d568fc4c842b8d4a930c50a655526a08fa7
ellson <devnull@localhost>
2007-12-26 19:00:14 +0000

    drop "mkdir -p" patch, hopefully recent libtools don't need it.

--- autogen.sh
+++ autogen.sh
@@ -9,8 +9,6 @@ touch config/depcomp
 #create an empty config/config.rpath else iconf configure test generates a warning
 touch config/config.rpath
 
-patch -N -p0 <ltmain.sh.patch
-                                                                                
 # ensure COPYING is based on cpl1.0.txt
 #   cpl1.0.txt was obtained from: http://www.opensource.org/licenses/cpl1.0.txt
 rm -f COPYING
