0736a04c8278e5d26a11ead5f82e0cea91491200
ellson <devnull@localhost>
2008-01-23 20:52:43 +0000

    Add smyrna to main graphviz tree

---
+++ lib/utilities/glTexFontTGA.h
@@ -0,0 +1,16 @@
+/*
+    Copyright (c) 1999 Nate Miller
+    
+    Notice: Usage of any code in this file is subject to the rules
+    described in the LICENSE.TXT file included in this directory.
+    Reading, compiling, or otherwise using this code constitutes
+    automatic acceptance of the rules in said text file.
+
+    File        -- glTexFontTGA.c
+    Date        -- 5/30/99
+    Author      -- Nate 'm|d' Miller
+    Contact     -- vandals1@home.com
+    Web         -- http://members.home.com/vandals1
+*/
+int fontLoadTGA (char *name, int id); /* id is the texture id to bind too */
+
