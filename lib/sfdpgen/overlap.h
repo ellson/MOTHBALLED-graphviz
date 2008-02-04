e2b76e3f3d3980ac66fade61fa02e1ad2a26e970
yifanhu <devnull@localhost>
2008-02-04 20:28:39 +0000

    added a new algorithm for remove node overlap, main code is overlap.c, which utilize functions in post_process.c

---
+++ lib/sfdpgen/overlap.h
@@ -0,0 +1,14 @@
+
+typedef  StressMajorizationSmoother OverlapSmoother;
+
+#define OverlapSmoother_struct StressMajorizationSmoother_struct
+
+void OverlapSmoother_delete(OverlapSmoother sm);
+
+OverlapSmoother OverlapSmoother_new(SparseMatrix A, int dim, real lambda0, real *x, real *width, int include_original_graph,
+				    real *max_overlap);
+
+void OverlapSmoother_smooth(OverlapSmoother sm, int dim, real *x);
+
+
+void remove_overlap(int dim, SparseMatrix A, real *x, real *label_sizes, int ntry, int *flag);
