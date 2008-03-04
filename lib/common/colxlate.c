dd7a95bc47b3fedfae3482dde3424582d247037f
arif <devnull@localhost>
2008-03-04 17:54:53 +0000

    *** empty log message ***

--- lib/common/colxlate.c
+++ lib/common/colxlate.c
@@ -35,21 +35,8 @@
 static char* colorscheme;
 
 #ifdef WIN32
-int strncasecmp(char *s1 , char *s2 ,size_t n)
-{
-  if (n == 0)
-    return 0;
-
-  while (n-- != 0 && tolower(*s1) == tolower(*s2))
-    {
-      if (n == 0 || *s1 == '\0' || *s2 == '\0')
-	break;
-      s1++;
-      s2++;
-    }
-
-  return tolower(*(unsigned char *) s1) - tolower(*(unsigned char *) s2);
-}
+extern int strcasecmp(const char *s1, const char *s2);
+extern int strncasecmp(const char *s1, const char *s2, unsigned int n);
 #endif
 
 
