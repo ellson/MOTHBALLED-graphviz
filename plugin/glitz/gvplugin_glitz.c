9dd560d439bcc9e923d2e963bd179ee07f364678
ellson <devnull@localhost>
2008-03-11 18:55:46 +0000

    make a start at a glitz plugin

---
+++ plugin/glitz/gvplugin_glitz.c
@@ -0,0 +1,26 @@
+/* $Id$ $Revision$ */
+/* vim:set shiftwidth=4 ts=8: */
+
+/**********************************************************
+*      This software is part of the graphviz package      *
+*                http://www.graphviz.org/                 *
+*                                                         *
+*            Copyright (c) 1994-2004 AT&T Corp.           *
+*                and is licensed under the                *
+*            Common Public License, Version 1.0           *
+*                      by AT&T Corp.                      *
+*                                                         *
+*        Information and Software Systems Research        *
+*              AT&T Research, Florham Park NJ             *
+**********************************************************/
+
+#include "gvplugin.h"
+
+extern gvplugin_installed_t gvdevice_types_glitz;
+
+static gvplugin_api_t apis[] = {
+    {API_device, &gvdevice_types_glitz},
+    {(api_t)0, 0},
+};
+
+gvplugin_library_t gvplugin_glitz_LTX_library = { "glitz", apis };
