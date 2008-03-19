1f87b6a9944b43c0de0732e2895042a014cb85db
erg <devnull@localhost>
2008-03-19 21:43:59 +0000

    Clean up smyrna files:   remove unnecessary globals   modify libraries not to rely on code in cmd/smyrna   remove static declarations from .h files   remove unnecessary libraries   mark unused code and clean up warnings

--- cmd/smyrna/gui/gui.h
+++ cmd/smyrna/gui/gui.h
@@ -32,25 +32,25 @@
 
 #define MAXIMUM_WIDGET_COUNT	97
 
-//GtkWidget *window1;		//main window
-extern GdkWindow* window1;
+//GtkWidget *window1;           //main window
+extern GdkWindow *window1;
 extern GtkWidget *statusbar1;
 
-extern GladeXML *xml;			//global libglade vars
+extern GladeXML *xml;		//global libglade vars
 extern GtkWidget *gladewidget;
 
 //1 subgraph 2 node 3 edge
 extern int frmObjectTypeIndex;
-extern Agraph_t* frmObjectg;
+extern Agraph_t *frmObjectg;
 
 
-extern GtkComboBox* cbSelectGraph;		//combo at top left
+extern GtkComboBox *cbSelectGraph;	//combo at top left
 
-extern GtkWidget* AttrWidgets[MAXIMUM_WIDGET_COUNT];	
-extern GtkWidget* AttrLabels[MAXIMUM_WIDGET_COUNT];	
+extern GtkWidget *AttrWidgets[MAXIMUM_WIDGET_COUNT];
+extern GtkWidget *AttrLabels[MAXIMUM_WIDGET_COUNT];
 extern int attr_widgets_modified[MAXIMUM_WIDGET_COUNT];
 extern int widgetcounter;	//number of attributes counted dynamically, might be removed in the future 
-extern attribute attr[MAXIMUM_WIDGET_COUNT];	
+extern attribute attr[MAXIMUM_WIDGET_COUNT];
 
 
 void create_object_properties();	//creates general purpose object properties template
@@ -59,22 +59,25 @@ void object_properties_edge_init();	//customize window for Edges
 void object_properties_cluster_init();	//customize window for Cluster
 void object_properties_graph_init();	//customize window for Graph , this shows the graph default values
 void graph_properties_init(int newgraph);	//initialize little open graph dialog
-GtkComboBox* get_SelectGraph();		//freaking GLADE!!!!!
-int update_graph_properties(Agraph_t* graph); //updates graph from gui
-void load_graph_properties(Agraph_t* graph);//load from graph to gui
+GtkComboBox *get_SelectGraph();	//freaking GLADE!!!!!
+int update_graph_properties(Agraph_t * graph);	//updates graph from gui
+void load_graph_properties(Agraph_t * graph);	//load from graph to gui
 
-void update_object_properties(int typeIndex,Agraph_t* g);	//updates objects from gui(node ,edge, cluster)
-int load_object_properties(int typeIndex,Agraph_t* g);	//load  from object to gui;
-void load_attributes();//loads attributes from a text file
-void change_selected_graph_attributes (Agraph_t* g,char* attrname,char* attrvalue);
-void change_selected_node_attributes (Agraph_t* g,char* attrname,char* attrvalue);
-void change_selected_edge_attributes (Agraph_t* g,char* attrname,char* attrvalue);
-char* get_attribute_string_value_from_widget(attribute* att);
+void update_object_properties(int typeIndex, Agraph_t * g);	//updates objects from gui(node ,edge, cluster)
+int load_object_properties(int typeIndex, Agraph_t * g);	//load  from object to gui;
+void load_attributes();		//loads attributes from a text file
+void change_selected_graph_attributes(Agraph_t * g, char *attrname,
+				      char *attrvalue);
+void change_selected_node_attributes(Agraph_t * g, char *attrname,
+				     char *attrvalue);
+void change_selected_edge_attributes(Agraph_t * g, char *attrname,
+				     char *attrvalue);
+char *get_attribute_string_value_from_widget(attribute * att);
 
 
 //GTK helpre functions
-//void Color_Widget_bg (int r, int g, int b, GtkWidget *widget);	//change background color 
- void Color_Widget_bg (char* colorstring, GtkWidget *widget);
+//void Color_Widget_bg (int r, int g, int b, GtkWidget *widget);        //change background color 
+void Color_Widget_bg(char *colorstring, GtkWidget * widget);
 
 
 
