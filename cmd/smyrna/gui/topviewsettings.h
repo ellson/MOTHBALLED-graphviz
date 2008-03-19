7283c0eb982a87ac5afe2897a78f470e078c1e24
erg <devnull@localhost>
2008-03-19 21:43:59 +0000

    Clean up smyrna files:   remove unnecessary globals   modify libraries not to rely on code in cmd/smyrna   remove static declarations from .h files   remove unnecessary libraries   mark unused code and clean up warnings

--- cmd/smyrna/gui/topviewsettings.h
+++ cmd/smyrna/gui/topviewsettings.h
@@ -1,17 +1,28 @@
+/* vim:set shiftwidth=4 ts=8: */
+
+/**********************************************************
+*      This software is part of the graphviz package      *
+*                http://www.graphviz.org/                 *
+*                                                         *
+*            Copyright (c) 1994-2004 AT&T Corp.           *
+*                and is licensed under the                *
+*            Common Public License, Version 1.0           *
+*                      by AT&T Corp.                      *
+*                                                         *
+*        Information and Software Systems Research        *
+*              AT&T Research, Florham Park NJ             *
+**********************************************************/
+
 #ifndef TOPVIEWSETTINGS_H
 #define TOPVIEWSETTINGS_H
 
-
 #include "smyrnadefs.h"
 
-
-_BB void on_settingsOKBtn_clicked (GtkWidget *widget,gpointer user_data);
-_BB void on_settingsCancelBtn_clicked (GtkWidget *widget,gpointer user_data);
-int load_settings_from_graph(Agraph_t *g);
-int update_graph_from_settings(Agraph_t *g);
-int show_settings_form();
-
-
-
+_BB void on_settingsOKBtn_clicked(GtkWidget * widget, gpointer user_data);
+_BB void on_settingsCancelBtn_clicked(GtkWidget * widget,
+				      gpointer user_data);
+extern int load_settings_from_graph(Agraph_t * g);
+extern int update_graph_from_settings(Agraph_t * g);
+extern int show_settings_form();
 
 #endif
