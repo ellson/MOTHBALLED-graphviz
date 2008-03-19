712ab532db7804d429aee8fc03d1ca1ebc27a99f
erg <devnull@localhost>
2008-03-19 21:43:58 +0000

    Clean up smyrna files:   remove unnecessary globals   modify libraries not to rely on code in cmd/smyrna   remove static declarations from .h files   remove unnecessary libraries   mark unused code and clean up warnings

--- cmd/smyrna/btree.h
+++ cmd/smyrna/btree.h
@@ -18,15 +18,16 @@
 #define	BTREE_H
 #include "tvnodes.h"
 
-btree_node* new_node(char* attribute,char* regex, float min, float max);
-int insert_node (btree_node* parent_n,btree_node* n);
-int delete_node(btree_node* n);
-int delete_node_recursive(btree_node* n);	//internal function
-btree_node* look_up_node_with_string(btree_node* n,char* string_to_lookup);
-int validate_lookup(btree_node* n,char* string_to_lookup);	//it can be anything, in this case attr_name
-int print_tree(btree_node* root);
-int print_children(btree_node* n);
-btree_node* tree_from_filter_string(char* filter_string);
-int evaluate_filter_atom(char* string,btree_node* Nodes[],char* op);
-int evaluate_expresions(tv_node* Node,btree_node* root);
+btree_node *new_node(char *attribute, char *regex, float min, float max);
+int insert_node(btree_node * parent_n, btree_node * n);
+int delete_node(btree_node * n);
+int delete_node_recursive(btree_node * n);	//internal function
+btree_node *look_up_node_with_string(btree_node * n,
+				     char *string_to_lookup);
+int validate_lookup(btree_node * n, char *string_to_lookup);	//it can be anything, in this case attr_name
+int print_tree(btree_node * root);
+int print_children(btree_node * n);
+btree_node *tree_from_filter_string(char *filter_string);
+int evaluate_filter_atom(char *string, btree_node * Nodes[], char *op);
+int evaluate_expresions(tv_node * Node, btree_node * root);
 #endif
