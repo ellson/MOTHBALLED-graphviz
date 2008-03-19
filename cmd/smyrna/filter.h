98d00bb62b0eeee60e204babad8c3a090521d4d9
erg <devnull@localhost>
2008-03-19 21:43:58 +0000

    Clean up smyrna files:   remove unnecessary globals   modify libraries not to rely on code in cmd/smyrna   remove static declarations from .h files   remove unnecessary libraries   mark unused code and clean up warnings

--- cmd/smyrna/filter.h
+++ cmd/smyrna/filter.h
@@ -18,17 +18,16 @@
 #define FILTER_H
 #include "btree.h"
 
-typedef struct _tv_filters
-{
-	tv_filter** filters;
-	int filter_count;
-}tv_filters;
+typedef struct _tv_filters {
+    tv_filter **filters;
+    int filter_count;
+} tv_filters;
 
-int clear_filter(tv_filter* f);
-int init_filters(tv_filters* filters);
-int add_filter_to_filters(tv_filters* filters,tv_filter* filter);
-int clear_filters(tv_filters* filters);
-int union_filter(tv_filter* f1,tv_filter* f2);
-int intersect_filter(tv_filter* f1,tv_filter* f2);
+int clear_filter(tv_filter * f);
+int init_filters(tv_filters * filters);
+int add_filter_to_filters(tv_filters * filters, tv_filter * filter);
+int clear_filters(tv_filters * filters);
+int union_filter(tv_filter * f1, tv_filter * f2);
+int intersect_filter(tv_filter * f1, tv_filter * f2);
 
 #endif
