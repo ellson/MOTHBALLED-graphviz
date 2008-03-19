cb800d7a8baf918c8c5e03055bf2d0f45b596793
erg <devnull@localhost>
2008-03-19 21:43:59 +0000

    Clean up smyrna files:   remove unnecessary globals   modify libraries not to rely on code in cmd/smyrna   remove static declarations from .h files   remove unnecessary libraries   mark unused code and clean up warnings

--- cmd/smyrna/gui/callbacks.h
+++ cmd/smyrna/gui/callbacks.h
@@ -22,46 +22,48 @@
 
 
 
-#ifdef	WIN32	//this shit is needed on WIN32 to get libglade see the callback
+#ifdef	WIN32			//this shit is needed on WIN32 to get libglade see the callback
 #define _BB  __declspec(dllexport)
 #else
-#define _BB  
+#define _BB
 #endif
 
-_BB void new_graph_clicked (GtkWidget *widget,gpointer user_data);
-_BB void open_graph_clicked (GtkWidget *widget,gpointer user_data);
-_BB void save_graph_clicked (GtkWidget     *widget,gpointer user_data);
-_BB void save_as_graph_clicked (GtkWidget     *widget,gpointer user_data);
-_BB void remove_graph_clicked (GtkWidget     *widget,gpointer user_data);
-void on_newNode (GtkWidget *button,gpointer user_data);
-void dlgOpenGraph_OK_Clicked( GtkWidget *button,gpointer data );
-_BB void btn_dot_clicked(GtkWidget *widget,gpointer user_data);
-_BB void btn_neato_clicked(GtkWidget *widget,gpointer user_data);
-_BB void btn_twopi_clicked(GtkWidget *widget,gpointer user_data);
-_BB void btn_circo_clicked(GtkWidget *widget,gpointer user_data);
-_BB void btn_fdp_clicked (GtkWidget *widget,gpointer user_data);
+_BB void new_graph_clicked(GtkWidget * widget, gpointer user_data);
+_BB void open_graph_clicked(GtkWidget * widget, gpointer user_data);
+_BB void save_graph_clicked(GtkWidget * widget, gpointer user_data);
+_BB void save_as_graph_clicked(GtkWidget * widget, gpointer user_data);
+_BB void remove_graph_clicked(GtkWidget * widget, gpointer user_data);
+void on_newNode(GtkWidget * button, gpointer user_data);
+void dlgOpenGraph_OK_Clicked(GtkWidget * button, gpointer data);
+_BB void btn_dot_clicked(GtkWidget * widget, gpointer user_data);
+_BB void btn_neato_clicked(GtkWidget * widget, gpointer user_data);
+_BB void btn_twopi_clicked(GtkWidget * widget, gpointer user_data);
+_BB void btn_circo_clicked(GtkWidget * widget, gpointer user_data);
+_BB void btn_fdp_clicked(GtkWidget * widget, gpointer user_data);
 
-_BB void graph_select_change (GtkWidget *widget,gpointer user_data);
-_BB void mGraphPropertiesSlot (GtkWidget *widget,gpointer user_data);
+_BB void graph_select_change(GtkWidget * widget, gpointer user_data);
+_BB void mGraphPropertiesSlot(GtkWidget * widget, gpointer user_data);
 
 
 //dlgOpenGraph btnOK clicked
-_BB void on_dlgOpenGraph_btnOK_clicked (GtkWidget *widget,gpointer user_data);
+_BB void on_dlgOpenGraph_btnOK_clicked(GtkWidget * widget,
+				       gpointer user_data);
 //dlgOpenGraph btncancelclicked
-_BB void on_dlgOpenGraph_btncancel_clicked (GtkWidget *widget,gpointer user_data);
+_BB void on_dlgOpenGraph_btncancel_clicked(GtkWidget * widget,
+					   gpointer user_data);
 
 //MENU 
 
 
 //frm Object OK button
 //hides frmObject , set changed values to selected objects
-_BB void frmObjectBtnOK_clicked (GtkWidget *widget,gpointer     user_data);
+_BB void frmObjectBtnOK_clicked(GtkWidget * widget, gpointer user_data);
 
 //frm Object Cancel button
 //hides frmObject , ignores changed values
 
-_BB void frmObjectBtnCancel_clicked (GtkWidget *widget,gpointer     user_data);
+_BB void frmObjectBtnCancel_clicked(GtkWidget * widget,
+				    gpointer user_data);
 
 
-_BB void attr_widgets_modifiedSlot(GtkWidget *widget,gpointer     user_data);
-
+_BB void attr_widgets_modifiedSlot(GtkWidget * widget, gpointer user_data);
