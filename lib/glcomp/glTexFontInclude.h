b2e1ead1f078376328508e419968ebe107ac6385
erg <devnull@localhost>
2008-03-20 15:38:34 +0000

    Move the glTex* files from utilities to glcompset.

---
+++ lib/glcomp/glTexFontInclude.h
@@ -0,0 +1,26 @@
+/*
+    Copyright (c) 1999 Nate Miller
+    
+    Notice: Usage of any code in this file is subject to the rules
+    described in the LICENSE.TXT file included in this directory.
+    Reading, compiling, or otherwise using this code constitutes
+    automatic acceptance of the rules in said text file.
+
+    File        -- glTexFontInclude.h
+    Date        -- 6/12/99
+    Author      -- Nate 'm|d' Miller
+    Contact     -- vandals1@home.com
+    Web         -- http://members.home.com/vandals1
+*/
+/* just generic headers to include, nothing fancy */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <math.h>
+#ifdef _WIN32
+#include <windows.h>
+#include <winuser.h>
+#include <tchar.h>
+#endif
+#include <GL/gl.h>
