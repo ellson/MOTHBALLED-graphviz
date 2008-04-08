a3886e9fa02eec458d1ac32817e214d11e86efcd
arif <devnull@localhost>
2008-04-08 17:36:37 +0000

    *** empty log message ***

--- cmd/smyrna/selection.h
+++ cmd/smyrna/selection.h
@@ -53,5 +53,6 @@ extern int deselect_all_nodes(Agraph_t * g);
 extern int deselect_all_edges(Agraph_t * g);
 extern int deselect_all_graphs(Agraph_t * g);
 extern int deselect_all(Agraph_t * g);
+int point_within_sphere_with_coords(float x0, float y0, float z0, float r,float x, float y,float z);
 
 #endif
