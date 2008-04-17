1faa7645564f036aa7c05b2191887e3cf4f6de3e
arif <devnull@localhost>
2008-04-17 15:35:47 +0000

    zoom abnormality is fixed, no more losing the sight of the graph while zooming

--- cmd/smyrna/glmotion.h
+++ cmd/smyrna/glmotion.h
@@ -5,4 +5,5 @@ int glmotion_main(ViewInfo * v,GdkEventMotion * event,GtkWidget * widget);
 int glmotion_zoom(ViewInfo * v);
 int glmotion_pan(ViewInfo * v);
 int glmotion_rotate(ViewInfo * v);
+int glmotion_adjust_pan(ViewInfo* v,float panx,float pany);
 #endif
