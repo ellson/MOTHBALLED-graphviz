dbd8a5676f5d1f389465cb80608e9e7aa372abac
ellson <devnull@localhost>
2008-04-25 20:06:20 +0000

    -Ttk plugin - 80% working

--- plugin/core/gvplugin_core.c
+++ plugin/core/gvplugin_core.c
@@ -17,33 +17,42 @@
 #include "gvplugin.h"
 
 extern gvplugin_installed_t gvdevice_dot_types;
-extern gvplugin_installed_t gvdevice_ps_types;
 extern gvplugin_installed_t gvdevice_fig_types;
-extern gvplugin_installed_t gvdevice_svg_types;
-extern gvplugin_installed_t gvdevice_vml_types;
 extern gvplugin_installed_t gvdevice_map_types;
+extern gvplugin_installed_t gvdevice_ps_types;
+extern gvplugin_installed_t gvdevice_svg_types;
+extern gvplugin_installed_t gvdevice_tk_types;
+extern gvplugin_installed_t gvdevice_vml_types;
+
 extern gvplugin_installed_t gvrender_dot_types;
-extern gvplugin_installed_t gvrender_ps_types;
 extern gvplugin_installed_t gvrender_fig_types;
-extern gvplugin_installed_t gvrender_svg_types;
-extern gvplugin_installed_t gvrender_vml_types;
 extern gvplugin_installed_t gvrender_map_types;
+extern gvplugin_installed_t gvrender_ps_types;
+extern gvplugin_installed_t gvrender_svg_types;
+extern gvplugin_installed_t gvrender_tk_types;
+extern gvplugin_installed_t gvrender_vml_types;
+
 extern gvplugin_installed_t gvloadimage_core_types;
 
 static gvplugin_api_t apis[] = {
     {API_device, &gvdevice_dot_types},
-    {API_device, &gvdevice_ps_types},
     {API_device, &gvdevice_fig_types},
-    {API_device, &gvdevice_svg_types},
-    {API_device, &gvdevice_vml_types},
     {API_device, &gvdevice_map_types},
+    {API_device, &gvdevice_ps_types},
+    {API_device, &gvdevice_svg_types},
+    {API_device, &gvdevice_tk_types},
+    {API_device, &gvdevice_vml_types},
+
     {API_render, &gvrender_dot_types},
-    {API_render, &gvrender_ps_types},
     {API_render, &gvrender_fig_types},
-    {API_render, &gvrender_svg_types},
-    {API_render, &gvrender_vml_types},
     {API_render, &gvrender_map_types},
+    {API_render, &gvrender_ps_types},
+    {API_render, &gvrender_svg_types},
+    {API_render, &gvrender_tk_types},
+    {API_render, &gvrender_vml_types},
+
     {API_loadimage, &gvloadimage_core_types},
+
     {(api_t)0, 0},
 };
 
