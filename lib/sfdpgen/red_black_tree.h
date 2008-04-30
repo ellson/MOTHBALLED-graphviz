6646c3f6065d3972f737157557a360381e1d9fcd
erg <devnull@localhost>
2008-04-30 20:55:00 +0000

    Install original sfdp files, with development logs; recommit new sfdp files

--- lib/sfdpgen/red_black_tree.h
+++ lib/sfdpgen/red_black_tree.h
@@ -1,86 +1,20 @@
-/* ------ misc.h --- */
-#include<stdio.h>
-#include<stdlib.h>
+/* vim:set shiftwidth=4 ts=8: */
 
-#ifndef INC_E_MISC_
-#define INC_E_MISC_
+/**********************************************************
+*      This software is part of the graphviz package      *
+*                http://www.graphviz.org/                 *
+*                                                         *
+*            Copyright (c) 1994-2004 AT&T Corp.           *
+*                and is licensed under the                *
+*            Common Public License, Version 1.0           *
+*                      by AT&T Corp.                      *
+*                                                         *
+*        Information and Software Systems Research        *
+*              AT&T Research, Florham Park NJ             *
+**********************************************************/
 
-
-/*  CONVENTIONS:  All data structures for red-black trees have the prefix */
-/*                "rb_" to prevent name conflicts. */
-/*                                                                      */
-/*                Function names: Each word in a function name begins with */
-/*                a capital letter.  An example funcntion name is  */
-/*                CreateRedTree(a,b,c). Furthermore, each function name */
-/*                should begin with a capital letter to easily distinguish */
-/*                them from variables. */
-/*                                                                     */
-/*                Variable names: Each word in a variable name begins with */
-/*                a capital letter EXCEPT the first letter of the variable */
-/*                name.  For example, int newLongInt.  Global variables have */
-/*                names beginning with "g".  An example of a global */
-/*                variable name is gNewtonsConstant. */
-
-void Assert(int assertion, char* error);
-void * SafeMalloc(size_t size);
-
-#endif
-
-
-
-
-
-
-
-
-
-/*--------------------- stack.h --------*/
-
-/*  CONVENTIONS:  All data structures for stacks have the prefix */
-/*                "stk_" to prevent name conflicts. */
-/*                                                                      */
-/*                Function names: Each word in a function name begins with */
-/*                a capital letter.  An example funcntion name is  */
-/*                CreateRedTree(a,b,c). Furthermore, each function name */
-/*                should begin with a capital letter to easily distinguish */
-/*                them from variables. */
-/*                                                                     */
-/*                Variable names: Each word in a variable name begins with */
-/*                a capital letter EXCEPT the first letter of the variable */
-/*                name.  For example, int newLongInt.  Global variables have */
-/*                names beginning with "g".  An example of a global */
-/*                variable name is gNewtonsConstant. */
-
-/*  if DATA_TYPE is undefined then stack.h and stack.c will be code for */
-/*  stacks of void *, if they are defined then they will be stacks of the */
-/*  appropriate data_type */
-
-#ifndef DATA_TYPE
-#define DATA_TYPE void *
-#endif
-
-typedef struct stk_stack_node {
-  DATA_TYPE info;
-  struct stk_stack_node * next;
-} stk_stack_node;
-
-typedef struct stk_stack { 
-  stk_stack_node * top;
-  stk_stack_node * tail;
-} stk_stack ;
-
-/*  These functions are all very straightforward and self-commenting so */
-/*  I didn't think additional comments would be useful */
-stk_stack * StackJoin(stk_stack * stack1, stk_stack * stack2);
-stk_stack * StackCreate();
-void StackPush(stk_stack * theStack, DATA_TYPE newInfoPointer);
-void * StackPop(stk_stack * theStack);
-int StackNotEmpty(stk_stack *);
-
-/*-------------------------------- red_black_tree.h ----*/
-#ifdef DMALLOC
-#include <dmalloc.h>
-#endif
+#ifndef RED_BLACK_TREE_H
+#define RED_BLACK_TREE_H
 
 /*  CONVENTIONS:  All data structures for red-black trees have the prefix */
 /*                "rb_" to prevent name conflicts. */
@@ -96,51 +30,47 @@ int StackNotEmpty(stk_stack *);
 /*                name.  For example, int newLongInt.  Global variables have */
 /*                names beginning with "g".  An example of a global */
 /*                variable name is gNewtonsConstant. */
-
-/* comment out the line below to remove all the debugging assertion */
-/* checks from the compiled code.  */
-#define DEBUG_ASSERT 1
 
 typedef struct rb_red_blk_node {
-  void* key;
-  void* info;
-  int red; /* if red=0 then the node is black */
-  struct rb_red_blk_node* left;
-  struct rb_red_blk_node* right;
-  struct rb_red_blk_node* parent;
+    void *key;
+    void *info;
+    int red;			/* if red=0 then the node is black */
+    struct rb_red_blk_node *left;
+    struct rb_red_blk_node *right;
+    struct rb_red_blk_node *parent;
 } rb_red_blk_node;
 
 
 /* Compare(a,b) should return 1 if *a > *b, -1 if *a < *b, and 0 otherwise */
 /* Destroy(a) takes a pointer to whatever key might be and frees it accordingly */
 typedef struct rb_red_blk_tree {
-  int (*Compare)(const void* a, const void* b); 
-  void (*DestroyKey)(void* a);
-  void (*DestroyInfo)(void* a);
-  void (*PrintKey)(const void* a);
-  void (*PrintInfo)(void* a);
-  /*  A sentinel is used for root and for nil.  These sentinels are */
-  /*  created when RBTreeCreate is caled.  root->left should always */
-  /*  point to the node which is the root of the tree.  nil points to a */
-  /*  node which should always be black but has aribtrary children and */
-  /*  parent and no key or info.  The point of using these sentinels is so */
-  /*  that the root and nil nodes do not require special cases in the code */
-  rb_red_blk_node* root;             
-  rb_red_blk_node* nil;              
+    int (*Compare) (const void *a, const void *b);
+    void (*DestroyKey) (void *a);
+    void (*DestroyInfo) (void *a);
+    void (*PrintKey) (const void *a);
+    void (*PrintInfo) (void *a);
+    /*  A sentinel is used for root and for nil.  These sentinels are */
+    /*  created when RBTreeCreate is caled.  root->left should always */
+    /*  point to the node which is the root of the tree.  nil points to a */
+    /*  node which should always be black but has aribtrary children and */
+    /*  parent and no key or info.  The point of using these sentinels is so */
+    /*  that the root and nil nodes do not require special cases in the code */
+    rb_red_blk_node *root;
+    rb_red_blk_node *nil;
 } rb_red_blk_tree;
 
-rb_red_blk_tree* RBTreeCreate(int  (*CompFunc)(const void*, const void*),
-			     void (*DestFunc)(void*), 
-			     void (*InfoDestFunc)(void*), 
-			     void (*PrintFunc)(const void*),
-			     void (*PrintInfo)(void*));
-rb_red_blk_node * RBTreeInsert(rb_red_blk_tree*, void* key, void* info);
-void RBTreePrint(rb_red_blk_tree*);
-void RBDelete(rb_red_blk_tree* , rb_red_blk_node* );
-void RBTreeDestroy(rb_red_blk_tree*);
-rb_red_blk_node* TreePredecessor(rb_red_blk_tree*,rb_red_blk_node*);
-rb_red_blk_node* TreeSuccessor(rb_red_blk_tree*,rb_red_blk_node*);
-rb_red_blk_node* RBExactQuery(rb_red_blk_tree*, void*);
-stk_stack * RBEnumerate(rb_red_blk_tree* tree,void* low, void* high);
-void NullFunction(void*);
+rb_red_blk_tree *RBTreeCreate(int (*CompFunc) (const void *, const void *),
+			      void (*DestFunc) (void *),
+			      void (*InfoDestFunc) (void *),
+			      void (*PrintFunc) (const void *),
+			      void (*PrintInfo) (void *));
+rb_red_blk_node *RBTreeInsert(rb_red_blk_tree *, void *key, void *info);
+void RBTreePrint(rb_red_blk_tree *);
+void RBDelete(rb_red_blk_tree *, rb_red_blk_node *);
+void RBTreeDestroy(rb_red_blk_tree *);
+rb_red_blk_node *TreePredecessor(rb_red_blk_tree *, rb_red_blk_node *);
+rb_red_blk_node *TreeSuccessor(rb_red_blk_tree *, rb_red_blk_node *);
+rb_red_blk_node *RBExactQuery(rb_red_blk_tree *, void *);
+void NullFunction(void *);
 
+#endif
