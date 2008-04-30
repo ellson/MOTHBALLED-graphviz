6de1bb1ca69fcc9b490ba8b3bb1c5d64cedbe290
erg <devnull@localhost>
2008-04-30 20:55:00 +0000

    Install original sfdp files, with development logs; recommit new sfdp files

--- lib/sfdpgen/LinkedList.c
+++ lib/sfdpgen/LinkedList.c
@@ -1,40 +1,63 @@
-#include "general.h"
+/* vim:set shiftwidth=4 ts=8: */
+
+/**********************************************************
+*      This software is part of the graphviz package      *
+*                http://www.graphviz.org/                 *
+*                                                         *
+*            Copyright (c) 1994-2004 AT&T Corp.           *
+*                and is licensed under the                *
+*            Common Public License, Version 1.0           *
+*                      by AT&T Corp.                      *
+*                                                         *
+*        Information and Software Systems Research        *
+*              AT&T Research, Florham Park NJ             *
+**********************************************************/
+
 #include "LinkedList.h"
+#include "memory.h"
 
-
-SingleLinkedList SingleLinkedList_new(void *data){
-  SingleLinkedList head;
-  head = MALLOC(sizeof(struct SingleLinkedList_struct));
-  head->data = data;
-  head->next = NULL;
-  return head;
+SingleLinkedList *SingleLinkedList_new(void *data)
+{
+    SingleLinkedList *head;
+    head = GNEW(SingleLinkedList);
+    head->data = data;
+    head->next = NULL;
+    return head;
 }
 
-void SingleLinkedList_delete(SingleLinkedList head,  void (*linklist_deallocator)(void*)){
-  SingleLinkedList next;
+void SingleLinkedList_delete(SingleLinkedList * head,
+			     void (*linklist_deallocator) (void *))
+{
+    SingleLinkedList *next;
 
-  if (!head) return;
-  do {
-    next = head->next;
-    if (head->data) linklist_deallocator(head->data);
-    if (head) FREE(head);
-    head = next;
-  } while (head);
+    if (!head)
+	return;
+    do {
+	next = head->next;
+	if (head->data)
+	    linklist_deallocator(head->data);
+	if (head)
+	    free(head);
+	head = next;
+    } while (head);
 
 }
 
 
-SingleLinkedList SingleLinkedList_prepend(SingleLinkedList l, void *data){
-  SingleLinkedList head = SingleLinkedList_new(data);
-  head->next = l;
-  return head;
+SingleLinkedList *SingleLinkedList_prepend(SingleLinkedList * l,
+					   void *data)
+{
+    SingleLinkedList *head = SingleLinkedList_new(data);
+    head->next = l;
+    return head;
 }
 
-void* SingleLinkedList_get_data(SingleLinkedList l){
-  return l->data;
+void *SingleLinkedList_get_data(SingleLinkedList * l)
+{
+    return l->data;
 }
 
-SingleLinkedList SingleLinkedList_get_next(SingleLinkedList l){
-  return l->next;
+SingleLinkedList *SingleLinkedList_get_next(SingleLinkedList * l)
+{
+    return l->next;
 }
-
--- lib/sfdpgen/LinkedList.h
+++ lib/sfdpgen/LinkedList.h
@@ -1,19 +1,34 @@
+/* vim:set shiftwidth=4 ts=8: */
+
+/**********************************************************
+*      This software is part of the graphviz package      *
+*                http://www.graphviz.org/                 *
+*                                                         *
+*            Copyright (c) 1994-2004 AT&T Corp.           *
+*                and is licensed under the                *
+*            Common Public License, Version 1.0           *
+*                      by AT&T Corp.                      *
+*                                                         *
+*        Information and Software Systems Research        *
+*              AT&T Research, Florham Park NJ             *
+**********************************************************/
+
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
-typedef struct SingleLinkedList_struct* SingleLinkedList;
+typedef struct SingleLinkedList_s {
+    void *data;
+    struct SingleLinkedList_s *next;
+} SingleLinkedList;
 
-struct SingleLinkedList_struct {
-  void *data;
-  SingleLinkedList next;
-};
+SingleLinkedList *SingleLinkedList_new(void *data);
+void SingleLinkedList_delete(SingleLinkedList * head,
+			     void (*linklist_deallocator) (void *));
+SingleLinkedList *SingleLinkedList_prepend(SingleLinkedList * l,
+					   void *data);
 
-SingleLinkedList SingleLinkedList_new(void *data);
-void SingleLinkedList_delete(SingleLinkedList head, void (*linklist_deallocator)(void*));
-SingleLinkedList SingleLinkedList_prepend(SingleLinkedList l, void *data);
+void *SingleLinkedList_get_data(SingleLinkedList * l);
 
-void* SingleLinkedList_get_data(SingleLinkedList l);
-
-SingleLinkedList SingleLinkedList_get_next(SingleLinkedList l);
+SingleLinkedList *SingleLinkedList_get_next(SingleLinkedList * l);
 
 #endif
