a4465f8880759132edd4216f2c73d571020cd322
erg <devnull@localhost>
2008-07-24 17:15:01 +0000

    Get rid of ^L characters as well.

--- lib/gvc/regex_win32.h
+++ lib/gvc/regex_win32.h
@@ -179,7 +179,7 @@ extern "C" {
        stored in the pattern buffer, so changing this does not affect
        already-compiled regexps.  */
     extern reg_syntax_t re_syntax_options;
-    
+    
     /* Define combinations of the above bits for the standard possibilities.
        (The [[[ comments delimit what gets put into the Texinfo file, so
        don't delete them!)  */
@@ -248,7 +248,7 @@ extern "C" {
    | RE_NO_BK_PARENS        | RE_NO_BK_REFS                             \
    | RE_NO_BK_VBAR          | RE_UNMATCHED_RIGHT_PAREN_ORD)
     /* [[[end syntaxes]]] */
-    
+    
     /* Maximum number of duplicates an interval can allow.  Some systems
        (erroneously) define this in other header files, but we want our
        value, so remove any previous define.  */
@@ -319,7 +319,7 @@ extern "C" {
         REG_ESIZE,              /* Compiled pattern bigger than 2^16 bytes.  */
         REG_ERPAREN             /* Unmatched ) or \); not returned from regcomp.  */
     } reg_errcode_t;
-    
+    
     /* This data structure represents a compiled pattern.  Before calling
        the pattern compiler, the fields `buffer', `allocated', `fastmap',
        `translate', and `no_sub' can be set.  After the pattern has been
@@ -399,7 +399,7 @@ extern "C" {
     };
 
     typedef struct re_pattern_buffer regex_t;
-    
+    
     /* Type for byte offsets within the string.  POSIX mandates this.  */
     typedef int regoff_t;
 
@@ -430,7 +430,7 @@ extern "C" {
         regoff_t rm_so;  /* Byte offset from string's start to substring's start.  */
         regoff_t rm_eo;  /* Byte offset from string's start to substring's end.  */
     } regmatch_t;
-    
+    
     /* Declarations for routines.  */
 
     /* To avoid duplicating every routine declaration -- once with a
@@ -538,7 +538,7 @@ extern "C" {
 #endif  /* C++ */
 
 #endif /* not __REGEXP_LIBRARY_H__ */
-
+
 /*
   Local variables:
   make-backup-files: t
