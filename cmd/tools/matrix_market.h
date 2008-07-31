408b555d9a3747f92e1242be4f3a75258b6540c5
erg <devnull@localhost>
2008-07-31 22:08:21 +0000

    *** empty log message ***

--- cmd/tools/matrix_market.h
+++ cmd/tools/matrix_market.h
@@ -1,5 +1,26 @@
+/* $Id$Revision:  */
+/* vim:set shiftwidth=4 ts=8: */
+
+/**********************************************************
+*      This software is part of the graphviz package      *
+*                http://www.graphviz.org/                 *
+*                                                         *
+*            Copyright (c) 1994-2004 AT&T Corp.           *
+*                and is licensed under the                *
+*            Common Public License, Version 1.0           *
+*                      by AT&T Corp.                      *
+*                                                         *
+*        Information and Software Systems Research        *
+*              AT&T Research, Florham Park NJ             *
+**********************************************************/
+#ifndef MATRIX_MARKET_H
+#define MATRIX_MARKET_H
+
 #include "mmio.h"
 #include "SparseMatrix.h"
 int mm_get_type(MM_typecode typecode);
-void SparseMatrix_export_matrix_market(FILE *file, SparseMatrix A, char *comment);
-SparseMatrix SparseMatrix_import_matrix_market(FILE *f, int format);
+void SparseMatrix_export_matrix_market(FILE * file, SparseMatrix A,
+				       char *comment);
+SparseMatrix SparseMatrix_import_matrix_market(FILE * f, int format);
+
+#endif
