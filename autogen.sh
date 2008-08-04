fa5adce187719e2ac32d5e24d2d60840bfea3439
ellson <devnull@localhost>
2008-08-04 20:38:30 +0000

    repair damage to autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,19 @@
 #! /bin/sh
 
-autoreconf - v-- install-- force || exit 1
+autoreconf -v --install --force || exit 1
+
 # ensure config/depcomp exists even if still using automake-1.4
 # otherwise "make dist" fails.
-    touch config / depcomp
+touch config/depcomp
+
 #create an empty config/config.rpath else iconf configure test generates a warning
-    touch config / config.rpath
+touch config/config.rpath
+
 # ensure COPYING is based on cpl1.0.txt
-#   cpl1.0.txt was obtained from: http:	//www.opensource.org/licenses/cpl1.0.txt
-    rm - f COPYING cp cpl1 .0.txt COPYING
+#   cpl1.0.txt was obtained from: http://www.opensource.org/licenses/cpl1.0.txt
+rm -f COPYING
+cp cpl1.0.txt COPYING
+
 # don't use any old cache, but create a new one
-    rm - f config.cache. / configure - C "$@"
+rm -f config.cache
+./configure -C "$@"
