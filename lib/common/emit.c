4d5ffd7a34884cd1b827e0fbf007453090e12afc
ellson <devnull@localhost>
2008-08-06 22:25:56 +0000

    off-by-one error in calculation of clip region

--- lib/common/emit.c
+++ lib/common/emit.c
@@ -850,10 +850,10 @@ static void setup_page(GVJ_t * job, graph_t * g)
     job->pageOffset.x = - job->pageSize.x * pagesArrayElem.x;
     job->pageOffset.y = - job->pageSize.y * pagesArrayElem.y;
 #endif
-    job->clip.LL.x = job->focus.x + job->pageSize.x * (pagesArrayElem.x - pagesArraySize.x / 2.);
-    job->clip.LL.y = job->focus.y + job->pageSize.y * (pagesArrayElem.y - pagesArraySize.y / 2.);
-    job->clip.UR.x = job->clip.LL.x + job->pageSize.x;
-    job->clip.UR.y = job->clip.LL.y + job->pageSize.y;
+    job->clip.LL.x = job->focus.x + job->pageSize.x * (pagesArrayElem.x - pagesArraySize.x / 2.) - 1;
+    job->clip.LL.y = job->focus.y + job->pageSize.y * (pagesArrayElem.y - pagesArraySize.y / 2.) - 1;
+    job->clip.UR.x = job->clip.LL.x + job->pageSize.x + 1;
+    job->clip.UR.y = job->clip.LL.y + job->pageSize.y + 1;
 
     /* CAUTION - job->translation was difficult to get right. */
     /* Test with and without assymetric margins, e.g: -Gmargin="1,0" */
