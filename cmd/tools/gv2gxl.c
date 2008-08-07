0847b09b6e032ea3d2afc8353283d07fc8a0e153
ellson <devnull@localhost>
2008-08-07 17:17:46 +0000

    Removing all existing usage of USE_CGRAPH     cgraph is now in full production in all these files
    configure.ac still contains the --with-cgraph option in case Arif wants to use it.

--- cmd/tools/gv2gxl.c
+++ cmd/tools/gv2gxl.c
@@ -15,10 +15,7 @@
 **********************************************************/
 
 
-#include <convert.h>
-#ifndef USE_CGRAPH
-#include <aghdr.h>
-#endif
+#include "convert.h"
 #include <ctype.h>
 
 #define SMALLBUF    128
@@ -725,22 +722,14 @@ static void writeBody(gxlstate_t * stp, Agraph_t * g, FILE * gxlFile)
 
     writeSubgs(stp, g, gxlFile);
     dd = (Agdatadict_t *) agdatadict(g);
-#ifdef USE_CGRAPH
     for (n = agfstnode(g); n; n = agnxtnode(g, n)) {
-#else
-    for (n = agfstnode(g); n; n = agnxtnode(n)) {
-#endif
 	realn = agidnode(stp->root, AGID(n), 0);
 	if (!writeval(realn)) {
 	    writeval(realn) = 1;
 	    writeNode(stp, n, gxlFile, dd->dict.n);
 	}
 
-#ifdef USE_CGRAPH
 	for (e = agfstout(g, n); e; e = agnxtout(g, e)) {
-#else
-	for (e = agfstout(n); e; e = agnxtout(e)) {
-#endif
 	    if (writeEdgeTest(g, e))
 		writeEdge(stp, e, gxlFile, dd->dict.e);
 	}
@@ -781,11 +770,7 @@ static void iterateBody(gxlstate_t * stp, Agraph_t * g)
     Agedge_t *e;
 
     iterate_subgs(stp, g);
-#ifdef USE_CGRAPH
     for (n = agfstnode(g); n; n = agnxtnode(g, n)) {
-#else
-    for (n = agfstnode(g); n; n = agnxtnode(n)) {
-#endif
 	char *gxlId;
 	char *nodename = agnameof(n);
 
@@ -800,11 +785,7 @@ static void iterateBody(gxlstate_t * stp, Agraph_t * g)
 	    addToMap(stp->nodeMap, nodename, gxlId);
 	}
 
-#ifdef USE_CGRAPH
 	for (e = agfstout(g, n); e; e = agnxtout(g, e)) {
-#else
-	for (e = agfstout(n); e; e = agnxtout(e)) {
-#endif
 	    if (writeEdgeTest(g, e)) {
 		char *edge_id = agget(e, GXL_ID);
 		if (!EMPTY(edge_id))
