fc3d342979f09267d4e0ad1f31696bccd9a43035
ellson <devnull@localhost>
2008-08-20 01:53:50 +0000

    mostly reverting previous change - found an easier way

---
+++ tclpkg/tclstubs/tclStubLib.c
@@ -0,0 +1,24 @@
+/* $Id$ $Revision$ */
+/* vim:set shiftwidth=4 ts=8: */
+
+/**********************************************************
+*      This software is part of the graphviz package      *
+*                http://www.graphviz.org/                 *
+*                                                         *
+*            Copyright (c) 1994-2004 AT&T Corp.           *
+*                and is licensed under the                *
+*            Common Public License, Version 1.0           *
+*                      by AT&T Corp.                      *
+*                                                         *
+*        Information and Software Systems Research        *
+*              AT&T Research, Florham Park NJ             *
+**********************************************************/
+
+/* this dummy file is here to fool libtool and automake */
+#include "tcl.h"
+
+/* need to reference an entry point to fool Sun's ld */
+void tclstubs_dummy(void)
+{
+    Tcl_InitStubs(0, 0, 0);
+}
