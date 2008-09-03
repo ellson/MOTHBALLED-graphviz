f50af73075c65551e6908f28cfd3d56971c4f47f
ellson <devnull@localhost>
2008-09-03 16:48:37 +0000

    lasi fixes

--- plugin/lasi/gvrender_lasi.cpp
+++ plugin/lasi/gvrender_lasi.cpp
@@ -32,7 +32,25 @@
 using namespace LASi;
 using namespace std;
 
+/* for CHAR_LATIN1  */
+#include "const.h"
+
+/*
+ *     J$: added `pdfmark' URL embedding.  PostScript rendered from
+ *         dot files with URL attributes will get active PDF links
+ *         from Adobe's Distiller.
+ */
+#define PDFMAX  14400           /*  Maximum size of PDF page  */
+
+extern "C" {
+	extern void epsf_define(FILE * of);
+	extern char *ps_string(char *ins, int latin);
+}
+
 typedef enum { FORMAT_PS, FORMAT_PS2, FORMAT_EPS } format_type;
+
+static int isLatin1;
+static char setupLatin1;
 
 PostscriptDocument doc;
 
@@ -97,6 +115,10 @@ static void lasi_begin_job(GVJ_t * job)
 //    gvdevice_printf(job, "%%%%Creator: %s version %s (%s)\n",
 //	    job->common->info[0], job->common->info[1], job->common->info[2]);
 //    gvdevice_printf(job, "%%%%For: %s\n", job->common->user);
+    doc.osHeader() << "%%Creator: " << job->common->info[0] \
+		   << " version " << job->common->info[1] \
+		   << " (" << job->common->info[2] << ")" << endl;
+    doc.osHeader() << "%%For: " << job->common->user << endl;
 }
 
 static void lasi_end_job(GVJ_t * job)
@@ -126,7 +148,7 @@ static void lasi_begin_graph(GVJ_t * job)
 {
     obj_state_t *obj = job->obj;
 
-//    setupLatin1 = FALSE;
+    setupLatin1 = FALSE;
 
     if (job->common->viewNum == 0) {
 //	gvdevice_printf(job, "%%%%Title: %s\n", obj->u.g->name);
@@ -157,6 +179,7 @@ static void lasi_begin_graph(GVJ_t * job)
 //        cat_preamble(job, job->common->lib);
         cat_libfile(job, job->common->lib, ps_txt);
 	/* include epsf */
+// FIXME!!
 //        epsf_define(job->output_file);
         if (job->common->show_boxes) {
             const char* args[2];
@@ -165,54 +188,73 @@ static void lasi_begin_graph(GVJ_t * job)
             cat_libfile(job, NULL, args);
         }
     }
-//    isLatin1 = (GD_charset(obj->u.g) == CHAR_LATIN1);
-//    /* We always setup Latin1. The charset info is always output,
-//     * and installing it is cheap. With it installed, we can then
-//     * rely on ps_string to convert UTF-8 characters whose encoding
-//     * is in the range of Latin-1 into the Latin-1 equivalent and
-//     * get the expected PostScript output.
-//     */
-//    if (!setupLatin1) {
+    isLatin1 = (GD_charset(obj->u.g) == CHAR_LATIN1);
+    /* We always setup Latin1. The charset info is always output,
+     * and installing it is cheap. With it installed, we can then
+     * rely on ps_string to convert UTF-8 characters whose encoding
+     * is in the range of Latin-1 into the Latin-1 equivalent and
+     * get the expected PostScript output.
+     */
+    if (!setupLatin1) {
+// FIXME!!
 //	gvdevice_fputs(job, "setupLatin1\n");	/* as defined in ps header */
 //	setupLatin1 = TRUE;
-//    }
+    }
     /*  Set base URL for relative links (for Distiller >= 3.0)  */
-//    if (obj->url)
+    if (obj->url) {
 //	gvdevice_printf(job, "[ {Catalog} << /URI << /Base (%s) >> >>\n"
 //		"/PUT pdfmark\n", obj->url);
-//	doc.osBody() << "[ {Catalog} << /URI << /Base (" << obj->url << ") >> >>" << endl;
-//	doc.osBody() << "/PUT pdfmark" << endl;
+	doc.osBody() << "[ {Catalog} << /URI << /Base (" << obj->url << ") >> >>" << endl;
+	doc.osBody() << "/PUT pdfmark" << endl;
+    }
 }
 
-#if 0
 static void lasi_begin_layer(GVJ_t * job, char *layername, int layerNum, int numLayers)
 {
-    gvdevice_printf(job, "%d %d setlayer\n", layerNum, numLayers);
+//    gvdevice_printf(job, "%d %d setlayer\n", layerNum, numLayers);
+    doc.osBody() << layerNum << ' ' << numLayers << "setlayer" << endl;
 }
 
 static void lasi_begin_page(GVJ_t * job)
 {
     box pbr = job->pageBoundingBox;
 
-    gvdevice_printf(job, "%%%%Page: %d %d\n",
-	    job->common->viewNum + 1, job->common->viewNum + 1);
+//    gvdevice_printf(job, "%%%%Page: %d %d\n",
+//	    job->common->viewNum + 1, job->common->viewNum + 1);
+    doc.osBody() << "%%Page: " << (job->common->viewNum + 1) << ' ' << (job->common->viewNum + 1) << endl;
     if (job->common->show_boxes == NULL)
-        gvdevice_printf(job, "%%%%PageBoundingBox: %d %d %d %d\n",
-	    pbr.LL.x, pbr.LL.y, pbr.UR.x, pbr.UR.y);
-    gvdevice_printf(job, "%%%%PageOrientation: %s\n",
-	    (job->rotation ? "Landscape" : "Portrait"));
+//	gvdevice_printf(job, "%%%%PageBoundingBox: %d %d %d %d\n",
+//	    pbr.LL.x, pbr.LL.y, pbr.UR.x, pbr.UR.y);
+	doc.osBody() << "%%PageBoundingBox: " << pbr.LL.x << ' ' \
+					      << pbr.LL.y << ' ' \
+					      << pbr.UR.x << ' ' \
+					      << pbr.UR.y << endl;
+//    gvdevice_printf(job, "%%%%PageOrientation: %s\n",
+//	    (job->rotation ? "Landscape" : "Portrait"));
+    doc.osBody() << "%%PageOrientation: " << (job->rotation ? "Landscape" : "Portrait") << endl;
     if (job->render.id == FORMAT_PS2)
-        gvdevice_printf(job, "<< /PageSize [%d %d] >> setpagedevice\n",
-            pbr.UR.x, pbr.UR.y);
-    gvdevice_printf(job, "%d %d %d beginpage\n",
-	    job->pagesArrayElem.x, job->pagesArrayElem.y, job->numPages);
+//	gvdevice_printf(job, "<< /PageSize [%d %d] >> setpagedevice\n",
+//	    pbr.UR.x, pbr.UR.y);
+	doc.osBody() << "<< /PageSize [" << pbr.UR.x << ' ' \
+					 << pbr.UR.y << "]  >> setpagedevice" << endl;
+//    gvdevice_printf(job, "%d %d %d beginpage\n",
+//	job->pagesArrayElem.x, job->pagesArrayElem.y, job->numPages);
+    doc.osBody() << job->pagesArrayElem.x << ' ' << job->pagesArrayElem.y << ' ' << job->numPages \
+		 << " beginpage" << endl;
     if (job->common->show_boxes == NULL)
-        gvdevice_printf(job, "gsave\n%d %d %d %d boxprim clip newpath\n",
-	    pbr.LL.x, pbr.LL.y, pbr.UR.x-pbr.LL.x, pbr.UR.y-pbr.LL.y);
-    gvdevice_printf(job, "%g %g set_scale %d rotate %g %g translate\n",
-	    job->scale.x, job->scale.y,
-	    job->rotation,
-	    job->translation.x, job->translation.y);
+//	gvdevice_printf(job, "gsave\n%d %d %d %d boxprim clip newpath\n",
+//	    pbr.LL.x, pbr.LL.y, pbr.UR.x-pbr.LL.x, pbr.UR.y-pbr.LL.y);
+	doc.osBody() << "gsave" << endl;
+	doc.osBody() << pbr.LL.x << ' ' << pbr.LL.y << ' ' \
+		     << pbr.UR.x << ' ' << pbr.UR.y << " boxprim clip newpath" << endl;
+	
+//    gvdevice_printf(job, "%g %g set_scale %d rotate %g %g translate\n",
+//	    job->scale.x, job->scale.y,
+//	    job->rotation,
+//	    job->translation.x, job->translation.y);
+    doc.osBody() << job->scale.x << ' ' << job->scale.y << " set_scale " \
+		 << job->rotation << " rotate " \
+		 << job->translation.x << ' ' << job->translation.y << " translate" << endl;
 
     /*  Define the size of the PS canvas  */
     if (job->render.id == FORMAT_PS2) {
@@ -220,58 +262,73 @@ static void lasi_begin_page(GVJ_t * job)
 	    job->common->errorfn("canvas size (%d,%d) exceeds PDF limit (%d)\n"
 		  "\t(suggest setting a bounding box size, see dot(1))\n",
 		  pbr.UR.x, pbr.UR.y, PDFMAX);
-	gvdevice_printf(job, "[ /CropBox [%d %d %d %d] /PAGES pdfmark\n",
-		pbr.LL.x, pbr.LL.y, pbr.UR.x, pbr.UR.y);
+//	gvdevice_printf(job, "[ /CropBox [%d %d %d %d] /PAGES pdfmark\n",
+//		pbr.LL.x, pbr.LL.y, pbr.UR.x, pbr.UR.y);
+    doc.osBody() << "[ /CropBox [" << pbr.LL.x << ' ' \
+				   << pbr.LL.y << ' ' \
+				   << pbr.UR.x << ' ' \
+				   << pbr.UR.y << "] /PAGES pdfmark" << endl;
     }
 }
 
 static void lasi_end_page(GVJ_t * job)
 {
     if (job->common->show_boxes) {
-	gvdevice_fputs(job, "0 0 0 edgecolor\n");
+//	gvdevice_fputs(job, "0 0 0 edgecolor\n");
+	doc.osBody() << "0 0 0 edgecolor" << endl;
 	cat_libfile(job, NULL, job->common->show_boxes + 1);
     }
     /* the showpage is really a no-op, but at least one PS processor
      * out there needs to see this literal token.  endpage does the real work.
      */
-    gvdevice_fputs(job, "endpage\nshowpage\ngrestore\n");
-    gvdevice_fputs(job, "%%PageTrailer\n");
-    gvdevice_printf(job, "%%%%EndPage: %d\n", job->common->viewNum);
+//    gvdevice_fputs(job, "endpage\nshowpage\ngrestore\n");
+//    gvdevice_fputs(job, "%%PageTrailer\n");
+//    gvdevice_printf(job, "%%%%EndPage: %d\n", job->common->viewNum);
+    doc.osBody() << "endpage" << endl;
+    doc.osBody() << "showpage" << endl;
+    doc.osBody() << "grestore" << endl;
+    doc.osBody() << "%%EndPage: " << job->common->viewNum << endl;
 }
 
 static void lasi_begin_cluster(GVJ_t * job)
 {
     obj_state_t *obj = job->obj;
 
-    gvdevice_printf(job, "%% %s\n", obj->u.sg->name);
-
-    gvdevice_fputs(job, "gsave\n");
+//    gvdevice_printf(job, "%% %s\n", obj->u.sg->name);
+//    gvdevice_fputs(job, "gsave\n");
+    doc.osBody() << "% " << obj->u.sg->name << endl;
+    doc.osBody() << "gsave" << endl;
 }
 
 static void lasi_end_cluster(GVJ_t * job)
 {
-    gvdevice_fputs(job, "grestore\n");
+//    gvdevice_fputs(job, "grestore\n");
+    doc.osBody() << "grestore" << endl;
 }
 
 static void lasi_begin_node(GVJ_t * job)
 {
-    gvdevice_fputs(job, "gsave\n");
+//    gvdevice_fputs(job, "gsave\n");
+    doc.osBody() << "gsave" << endl;
 }
 
 static void lasi_end_node(GVJ_t * job)
 {
-    gvdevice_fputs(job, "grestore\n");
+//    gvdevice_fputs(job, "grestore\n");
+    doc.osBody() << "grestore" << endl;
 }
 
 static void
 lasi_begin_edge(GVJ_t * job)
 {
-    gvdevice_fputs(job, "gsave\n");
+//    gvdevice_fputs(job, "gsave\n");
+    doc.osBody() << "gsave" << endl;
 }
 
 static void lasi_end_edge(GVJ_t * job)
 {
-    gvdevice_fputs(job, "grestore\n");
+//    gvdevice_fputs(job, "grestore\n");
+    doc.osBody() << "grestore" << endl;
 }
 
 static void lasi_begin_anchor(GVJ_t *job, char *url, char *tooltip, char *target)
@@ -279,25 +336,32 @@ static void lasi_begin_anchor(GVJ_t *job, char *url, char *tooltip, char *target
     obj_state_t *obj = job->obj;
 
     if (url && obj->url_map_p) {
-        gvdevice_fputs(job, "[ /Rect [ ");
-	gvdevice_printpointflist(job, obj->url_map_p, 2);
-        gvdevice_fputs(job, " ]\n");
-        gvdevice_printf(job, "  /Border [ 0 0 0 ]\n"
-		"  /Action << /Subtype /URI /URI %s >>\n"
-		"  /Subtype /Link\n"
-		"/ANN pdfmark\n",
-		ps_string(url, isLatin1));
+//	gvdevice_fputs(job, "[ /Rect [ ");
+//	gvdevice_printpointflist(job, obj->url_map_p, 2);
+//	gvdevice_fputs(job, " ]\n");
+//	gvdevice_printf(job, "  /Border [ 0 0 0 ]\n"
+//		"  /Action << /Subtype /URI /URI %s >>\n"
+//		"  /Subtype /Link\n"
+//		"/ANN pdfmark\n",
+//		ps_string(url, isLatin1));
+	doc.osBody() << "[ /Rect [ ";
+	lasi_printpointflist(job, obj->url_map_p, 2);
+	doc.osBody() << "]" << endl;
+	doc.osBody() << "  /Border [ 0 0 0 ]" << endl;
+//	doc.osBody() << "  /Action << /Subtype /URI /URI " << ps_string(url, isLatin1) << " >>" << endl;
+	doc.osBody() << "  /Action << /Subtype /URI /URI " << url << " >>" << endl;
+	doc.osBody() << "/ANN pdfmark" << endl;
     }
 }
 
-static void
-ps_set_pen_style(GVJ_t *job)
+static void ps_set_pen_style(GVJ_t *job)
 {
     double penwidth = job->obj->penwidth;
     char *p, *line, **s = job->obj->rawstyle;
 
-    gvdevice_printnum(job, penwidth);
-    gvdevice_fputs(job," setlinewidth\n");
+//    gvdevice_printnum(job, penwidth);
+//    gvdevice_fputs(job," setlinewidth\n");
+    doc.osBody() << penwidth << " setlinewidth" << endl;
 
     while (s && (p = line = *s++)) {
 	if (strcmp(line, "setlinewidth") == 0)
@@ -306,17 +370,18 @@ ps_set_pen_style(GVJ_t *job)
 	    p++;
 	p++;
 	while (*p) {
-            gvdevice_printf(job,"%s ", p);
+//	    gvdevice_printf(job,"%s ", p);
+	    doc.osBody() << p << ' ';
 	    while (*p)
 		p++;
 	    p++;
 	}
 	if (strcmp(line, "invis") == 0)
 	    job->obj->penwidth = 0;
-	gvdevice_printf(job, "%s\n", line);
+//	gvdevice_printf(job, "%s\n", line);
+	doc.osBody() << line << endl;
     }
 }
-#endif
 
 static void ps_set_color(GVJ_t *job, gvcolor_t *color)
 {
@@ -351,11 +416,13 @@ static void lasi_textpara(GVJ_t * job, pointf p, textpara_t * para)
     if (job->obj->pencolor.u.HSVA[3] < .5)
 	return;  /* skip transparent text */
 
+//fprintf(stderr,"font=\"%s\"\n", para->fontname);
+//fprintf(stderr,"ps_font=\"%s\"\n", para->postscript_alias->name);
+//fprintf(stderr,"svg_font=\"%s\"\n", para->postscript_alias->svg_font_family);
     ps_set_color(job, &(job->obj->pencolor));
 //    gvdevice_printnum(job, para->fontsize);
 //    gvdevice_printf(job, " /%s set_font\n", para->fontname);
-    doc.osBody() << setFont(para->fontname) << setFontSize(para->fontsize) << endl;
-//    str = ps_string(para->str,isLatin1);
+    doc.osBody() << setFont(para->postscript_alias->svg_font_family) << setFontSize(para->fontsize) << endl;
     switch (para->just) {
     case 'r':
         p.x -= para->width;
@@ -374,8 +441,10 @@ static void lasi_textpara(GVJ_t * job, pointf p, textpara_t * para)
     lasi_printpointf(job, p);
     doc.osBody() << "moveto" << endl;
 //    gvdevice_printnum(job, para->width);
+//    str = ps_string(para->str,isLatin1);
+    str = para->str;
 //    gvdevice_printf(job, " %s alignedtext\n", str);
-    doc.osBody() << show(para->str) << endl;
+    doc.osBody() << show(str) << endl;
 
 }
 
@@ -396,7 +465,7 @@ static void lasi_ellipse(GVJ_t * job, pointf * A, int filled)
         doc.osBody() << "ellipse_path fill" << endl;
     }
     if (job->obj->pencolor.u.HSVA[3] > .5) {
-//	ps_set_pen_style(job);
+	ps_set_pen_style(job);
 	ps_set_color(job, &(job->obj->pencolor));
 //	gvdevice_printpointflist(job, AA, 2);
 //	gvdevice_fputs(job, " ellipse_path stroke\n");
@@ -429,7 +498,7 @@ lasi_bezier(GVJ_t * job, pointf * A, int n, int arrow_at_start,
 	doc.osBody() << "closepath fill" << endl;
     }
     if (job->obj->pencolor.u.HSVA[3] > .5) {
-//	ps_set_pen_style(job);
+	ps_set_pen_style(job);
 	ps_set_color(job, &(job->obj->pencolor));
 //	gvdevice_fputs(job, "newpath ");
 //	gvdevice_printpointf(job, A[0]);
@@ -470,7 +539,7 @@ static void lasi_polygon(GVJ_t * job, pointf * A, int n, int filled)
 	doc.osBody() << "closepath fill" << endl;
     }
     if (job->obj->pencolor.u.HSVA[3] > .5) {
-//	ps_set_pen_style(job);
+	ps_set_pen_style(job);
 	ps_set_color(job, &(job->obj->pencolor));
 //	gvdevice_fputs(job, "newpath ");
 //	gvdevice_printpointf(job, A[0]);
@@ -494,7 +563,7 @@ static void lasi_polyline(GVJ_t * job, pointf * A, int n)
     int j;
 
     if (job->obj->pencolor.u.HSVA[3] > .5) {
-//	ps_set_pen_style(job);
+	ps_set_pen_style(job);
 	ps_set_color(job, &(job->obj->pencolor));
 //	gvdevice_fputs(job, "newpath ");
 //	gvdevice_printpointf(job, A[0]);
@@ -521,49 +590,55 @@ static void lasi_comment(GVJ_t * job, char *str)
     doc.osBody() << "% " << str << endl;
 }
 
-#if 0
 static void lasi_library_shape(GVJ_t * job, char *name, pointf * A, int n, int filled)
 {
     if (filled && job->obj->fillcolor.u.HSVA[3] > .5) {
 	ps_set_color(job, &(job->obj->fillcolor));
-	gvdevice_fputs(job, "[ ");
-        gvdevice_printpointflist(job, A, n);
-        gvdevice_fputs(job, " ");
-        gvdevice_printpointf(job, A[0]);
-	gvdevice_printf(job, " ]  %d true %s\n", n, name);
+//	gvdevice_fputs(job, "[ ");
+//	gvdevice_printpointflist(job, A, n);
+//	gvdevice_fputs(job, " ");
+//	gvdevice_printpointf(job, A[0]);
+//	gvdevice_printf(job, " ]  %d true %s\n", n, name);
+	doc.osBody() << "[ ";
+	lasi_printpointflist(job, A, n);
+	lasi_printpointf(job, A[0]);
+	doc.osBody() << "] " << n << " true " << name << endl;
     }
     if (job->obj->pencolor.u.HSVA[3] > .5) {
         ps_set_pen_style(job);
         ps_set_color(job, &(job->obj->pencolor));
-        gvdevice_fputs(job, "[ ");
-        gvdevice_printpointflist(job, A, n);
-        gvdevice_fputs(job, " ");
-        gvdevice_printpointf(job, A[0]);
-        gvdevice_printf(job, " ]  %d false %s\n", n, name);
+//	gvdevice_fputs(job, "[ ");
+//	gvdevice_printpointflist(job, A, n);
+//	gvdevice_fputs(job, " ");
+//	gvdevice_printpointf(job, A[0]);
+//	gvdevice_printf(job, " ]  %d false %s\n", n, name);
+	doc.osBody() << "[ ";
+	lasi_printpointflist(job, A, n);
+	lasi_printpointf(job, A[0]);
+	doc.osBody() << "] " << n << " false " << name << endl;
     }
 }
-#endif
 
 static gvrender_engine_t lasi_engine = {
     lasi_begin_job,
     lasi_end_job,
     lasi_begin_graph,
     0,				/* lasi_end_graph */
-0,//    lasi_begin_layer,
+    lasi_begin_layer,
     0,				/* lasi_end_layer */
-0,//    lasi_begin_page,
-0,//    lasi_end_page,
-0,//    lasi_begin_cluster,
-0,//    lasi_end_cluster,
+    lasi_begin_page,
+    lasi_end_page,
+    lasi_begin_cluster,
+    lasi_end_cluster,
     0,				/* lasi_begin_nodes */
     0,				/* lasi_end_nodes */
     0,				/* lasi_begin_edges */
     0,				/* lasi_end_edges */
-0,//    lasi_begin_node,
-0,//    lasi_end_node,
-0,//    lasi_begin_edge,
-0,//    lasi_end_edge,
-0,//    lasi_begin_anchor,
+    lasi_begin_node,
+    lasi_end_node,
+    lasi_begin_edge,
+    lasi_end_edge,
+    lasi_begin_anchor,
     0,				/* lasi_end_anchor */
     lasi_textpara,
     0,				/* lasi_resolve_color */
@@ -572,7 +647,7 @@ static gvrender_engine_t lasi_engine = {
     lasi_bezier,
     lasi_polyline,
     lasi_comment,
-0,//    lasi_library_shape,
+    lasi_library_shape,
 };
 
 static gvrender_features_t render_features_lasi = {
