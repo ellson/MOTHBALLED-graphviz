7e4141934c6ffb8b6c4013a994885666f66f24a3
ellson <devnull@localhost>
2008-09-26 14:20:55 +0000

    add hsva, cmyk outputs

--- lib/inkpot/inkpot_xlate.h
+++ lib/inkpot/inkpot_xlate.h
@@ -1,18 +1,18 @@
 /* $Id$ $Revision$ */
 /* vim:set shiftwidth=4 ts=8: */
 
-/**********************************************************
-*      This software is part of the graphviz package      *
-*                http://www.graphviz.org/                 *
-*                                                         *
-*            Copyright (c) 1994-2004 AT&T Corp.           *
-*                and is licensed under the                *
-*            Common Public License, Version 1.0           *
-*                      by AT&T Corp.                      *
-*                                                         *
-*        Information and Software Systems Research        *
-*              AT&T Research, Florham Park NJ             *
-**********************************************************/
+/***********************************************************
+ *      This software is part of the graphviz package      *
+ *                http://www.graphviz.org/                 *
+ *                                                         *
+ *            Copyright (c) 1994-2008 AT&T Corp.           *
+ *                and is licensed under the                *
+ *            Common Public License, Version 1.0           *
+ *                      by AT&T Corp.                      *
+ *                                                         *
+ *        Information and Software Systems Research        *
+ *              AT&T Research, Florham Park NJ             *
+ **********************************************************/
 
 #ifndef INKPOT_XLATE_H
 #define INKPOT_XLATE_H
@@ -21,24 +21,9 @@
 extern "C" {
 #endif
 
-/* possible representations of color in gvcolor_t */
-typedef enum { HSVA_DOUBLE, RGBA_BYTE, RGBA_WORD, CMYK_BYTE,
-                RGBA_DOUBLE, COLOR_STRING, COLOR_INDEX } color_type_t;
-
-/* gvcolor_t can hold a color spec in a choice or representations */
-typedef struct color_s {
-    union {
-        double RGBA[4];
-        double HSVA[4];
-        unsigned char rgba[4];
-        unsigned char cmyk[4];
-        int rrggbbaa[4];
-        char *string;
-        int index;
-    } u;
-    color_type_t type;
-} gvcolor_t;
-
+extern void hsva2rgba(double hsva[4], double rgba[4]);
+extern void rgba2hsva(double rgba[4], double hsva[4]);
+extern void rgba2cmyk(double rgba[4], double cmyk[4]);
 
 #ifdef __cplusplus
 }
