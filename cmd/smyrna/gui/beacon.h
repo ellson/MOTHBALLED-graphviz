e628c11ea19d4da35335cea58a49d0ff74bdce21
arif <devnull@localhost>
2008-10-14 23:16:42 +0000

    *** empty log message ***

---
+++ cmd/smyrna/gui/beacon.h
@@ -0,0 +1,11 @@
+#ifndef BEACON_H
+#define BEACON_H
+#include "smyrnadefs.h"
+
+int pick_node(topview_node * n);
+int is_node_picked(topview_node * n);
+int remove_from_pick_list(topview_node * n);
+int add_to_pick_list(topview_node * n);
+int draw_node_hint_boxes();
+
+#endif
