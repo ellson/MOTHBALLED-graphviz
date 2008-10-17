0d8e933464421f1304d61efd3a68b00fd87d6f29
erg <devnull@localhost>
2008-10-17 20:31:49 +0000

    Add new .h file for aspect ratio processing

---
+++ lib/dotgen/aspect.h
@@ -0,0 +1,37 @@
+/* $Id$Revision: */
+/* vim:set shiftwidth=4 ts=8: */
+
+/**********************************************************
+*      This software is part of the graphviz package      *
+*                http://www.graphviz.org/                 *
+*                                                         *
+*            Copyright (c) 1994-2004 AT&T Corp.           *
+*                and is licensed under the                *
+*            Common Public License, Version 1.0           *
+*                      by AT&T Corp.                      *
+*                                                         *
+*        Information and Software Systems Research        *
+*              AT&T Research, Florham Park NJ             *
+**********************************************************/
+
+#ifndef		ASPECT_H
+#define		ASPECT_H
+
+typedef struct aspect_t {
+    double targetAR;      /* target aspect ratio */
+    double combiAR;
+    int prevIterations;   /* no. of iterations in previous pass */
+    int curIterations;    /* no. of iterations in current pass */
+    int nextIter;         /* dynamically adjusted no. of iterations */
+    int nPasses;          /* bound on no. of top-level passes */
+    int badGraph;         /* hack: set if graph is disconnected or has
+                           * clusters. If so, turn off aspect */
+} aspect_t;
+
+extern void rank3(graph_t * g, aspect_t * asp);
+extern void initEdgeTypes(graph_t * g);
+extern void init_UF_size(graph_t * g);
+extern int countDummyNodes(graph_t * g);
+
+#endif				/* ASPECT_H */
+
