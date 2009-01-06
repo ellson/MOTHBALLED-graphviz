d8474b64fa65283e503821475ac6d13c28e51b41
erg <devnull@localhost>
2009-01-06 04:23:23 +0000

    Make sure fdp global parameters are statically initialized. This avoids making the algorithm independent of processing argv[].

--- lib/common/globals.c
+++ lib/common/globals.c
@@ -21,3 +21,16 @@
 #define EXTERN
 #include "types.h"
 #include "globals.h"
+
+/* Default layout values, possibly set via command line; -1 indicates unset */
+fdpParms_t fdp_parms = {
+    1,                          /* useGrid */
+    1,                          /* useNew */
+    -1,                         /* numIters */
+    50,                         /* unscaled */
+    0.0,                        /* C */
+    1.0,                        /* Tfact */
+    -1.0,                       /* K - set in initParams; used in init_edge */
+    -1.0,                       /* T0 */
+};
+
