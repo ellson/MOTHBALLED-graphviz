a7109b7bc2c83361d9c7d0da862ef58d2ce3183f
ellson <devnull@localhost>
2009-03-18 19:18:10 +0000

    rename config-versio.h -> graphviz_version.h, distribute and install it

--- windows/config.h
+++ windows/config.h
@@ -1,6 +1,6 @@
 /* manually generated configuration for Windows */
 
-#include "config_version.h"
+#include "graphviz_version.h"
 
 /* Command to open a browser on a URL */
 #define BROWSER "xdg-open"
