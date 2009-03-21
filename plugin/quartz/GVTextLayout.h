1ccc863ae3530d4ff2bdd8c04d42da3ad24eb6d6
glenlow <devnull@localhost>
2009-03-21 07:10:18 +0000

    standard AT&T copyright header

--- plugin/quartz/GVTextLayout.h
+++ plugin/quartz/GVTextLayout.h
@@ -1,10 +1,18 @@
-//
-//  GVTextLayout.h
-//  
-//
-//  Created by Glen Low on 3/01/09.
-//  Copyright 2009 Pixelglow Software. All rights reserved.
-//
+/* $Id$ $Revision$ */
+/* vim:set shiftwidth=4 ts=8: */
+
+/**********************************************************
+ *      This software is part of the graphviz package      *
+ *                http://www.graphviz.org/                 *
+ *                                                         *
+ *            Copyright (c) 1994-2008 AT&T Corp.           *
+ *                and is licensed under the                *
+ *            Common Public License, Version 1.0           *
+ *                      by AT&T Corp.                      *
+ *                                                         *
+ *        Information and Software Systems Research        *
+ *              AT&T Research, Florham Park NJ             *
+ **********************************************************/
 
 #import <UIKit/UIKit.h>
 
