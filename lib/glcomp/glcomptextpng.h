ff156f2f8967c84fe238a6693e60f730f4a91dff
arif <devnull@localhost>
2009-03-23 19:36:38 +0000

    licence issues

---
+++ lib/glcomp/glcomptextpng.h
@@ -0,0 +1,13 @@
+/**********************************************************
+*      This software is part of the graphviz package      *
+*                http://www.graphviz.org/                 *
+*                                                         *
+*            Copyright (c) 1994-2007 AT&T Corp.           *
+*                and is licensed under the                *
+*            Common Public License, Version 1.0           *
+*                      by AT&T Corp.                      *
+*                                                         *
+*        Information and Software Systems Research        *
+*              AT&T Research, Florham Park NJ             *
+**********************************************************/
+
