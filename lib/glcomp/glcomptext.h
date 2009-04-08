35789a499ad8e4076361d272d3c6a3c336bb2a70
arif <devnull@localhost>
2009-04-08 18:55:09 +0000

    smyrna improvements

--- lib/glcomp/glcomptext.h
+++ lib/glcomp/glcomptext.h
@@ -31,7 +31,8 @@ extern "C" {
 extern fontset_t* fontset_init(void);
 extern void free_font_set(fontset_t* fontset);
 extern void copy_font(glCompText* targetfont,const glCompText* sourcefont);
-extern int add_font(fontset_t* fontset,char* fontdesc);
+extern glCompText* add_font(fontset_t* fontset,char* fontdesc);
+extern glCompText* add_glut_font(fontset_t* fontset,void* glutfont);
 extern void glprintf (glCompText*, GLfloat , GLfloat , GLfloat, char*);
 extern	void glprintfglut (void* font, GLfloat xpos, GLfloat ypos, char *bf);
 
