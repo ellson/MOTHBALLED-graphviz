aeb85f0ffb840912dac60f4b3178322eb05f1a72
erg <devnull@localhost>
2009-05-12 19:21:23 +0000

    Clean up warning messages; fix various errors; use single function for smyrna data directory

--- cmd/smyrna/topview.h
+++ cmd/smyrna/topview.h
@@ -34,5 +34,6 @@ void local_zoom(topview * t);
 void originate_distorded_coordinates(topview * t);
 float calcfontsize(float totaledgelength,int totallabelsize,int edgecount,int totalnodecount);
 void select_with_regex(char* exp);
+void settvcolorinfo(Agraph_t* g,topview* t);
 
 #endif
