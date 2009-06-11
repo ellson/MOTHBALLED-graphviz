2fc5faa0c1d63fdf0d0f530ee5ca38fe1a32b1e5
ellson <devnull@localhost>
2009-06-11 20:55:07 +0000

    remove one of two glutinit() statements

--- cmd/smyrna/main.c
+++ cmd/smyrna/main.c
@@ -149,8 +149,6 @@ int main(int argc, char *argv[])
 {
     GdkGLConfig *glconfig;
     char* initFileName;
-	glutInit(&argc,argv);
-
 
     smyrnaDir = getenv ("SMYRNA_PATH");
     if (!smyrnaDir) {
