402f3993ce0ae4ddcd0c08d8448a993b210da4b5
erg <devnull@localhost>
2009-07-08 21:00:58 +0000

    Add gml to gv converter

---
+++ cmd/tools/gml2gv.c
@@ -0,0 +1,155 @@
+/* $Id$Revision: */
+/* vim:set shiftwidth=4 ts=8: */
+
+/**********************************************************
+*      This software is part of the graphviz package      *
+*                http://www.graphviz.org/                 *
+*                                                         *
+*            Copyright (c) 1994-2004 AT&T Corp.           *
+*                and is licensed under the                *
+*            Common Public License, Version 1.0           *
+*                      by AT&T Corp.                      *
+*                                                         *
+*        Information and Software Systems Research        *
+*              AT&T Research, Florham Park NJ             *
+**********************************************************/
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "gml2gv.h"
+#include <stdlib.h>
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+
+#ifdef HAVE_GETOPT_H
+#include <getopt.h>
+#else
+#include "compat_getopt.h"
+#endif
+
+static FILE *outFile;
+static char *CmdName;
+static char **Files;
+
+static FILE *getFile(void)
+{
+    FILE *rv = NULL;
+    static FILE *savef = NULL;
+    static int cnt = 0;
+
+    if (Files == NULL) {
+	if (cnt++ == 0) {
+	    rv = stdin;
+	}
+    } else {
+	if (savef)
+	    fclose(savef);
+	while (Files[cnt]) {
+	    if ((rv = fopen(Files[cnt++], "r")) != 0)
+		break;
+	    else
+		fprintf(stderr, "Can't open %s\n", Files[cnt - 1]);
+	}
+    }
+    savef = rv;
+    return rv;
+}
+
+static FILE *openFile(char *name, char *mode)
+{
+    FILE *fp;
+    char *modestr;
+
+    fp = fopen(name, mode);
+    if (!fp) {
+        if (*mode == 'r')
+            modestr = "reading";
+        else
+            modestr = "writing";
+        fprintf(stderr, "%s: could not open file %s for %s\n",
+                CmdName, name, modestr);
+        perror(name);
+        exit(1);
+    }
+    return fp;
+}
+
+
+static char *useString = "Usage: nop [-p?] <files>\n\
+  -o<file>  : output to <file> (stdout)\n\
+  -? - print usage\n\
+If no files are specified, stdin is used\n";
+
+static void usage(int v)
+{
+    printf(useString);
+    exit(v);
+}
+
+static char *cmdName(char *path)
+{
+    char *sp;
+
+    sp = strrchr(path, '/');
+    if (sp)
+        sp++;
+    else
+        sp = path;
+    return sp;
+}
+
+static void initargs(int argc, char **argv)
+{
+    int c;
+
+    CmdName = cmdName(argv[0]);
+    while ((c = getopt(argc, argv, ":gdo:?")) != -1) {
+	switch (c) {
+	case 'o':
+	    outFile = openFile(optarg, "w");
+	    break;
+	case '?':
+	    if (optopt == '?')
+		usage(0);
+	    else {
+		fprintf(stderr, "%s: option -%c unrecognized\n", CmdName,
+			optopt);
+		exit(1);
+	    }
+	}
+    }
+
+    argv += optind;
+    argc -= optind;
+
+    if (argc)
+	Files = argv;
+    if (!outFile)
+	outFile = stdout;
+}
+
+int main(int argc, char **argv)
+{
+    Agraph_t *G;
+    Agraph_t *prev = 0;
+    FILE *inFile;
+    int cnt, rv;
+
+    rv = 0;
+    initargs(argc, argv);
+    while ((inFile = getFile())) {
+	cnt = 0;
+	while ((G = gml_to_gv(inFile, cnt, &rv))) {
+	    cnt++;
+	    if (prev)
+		agclose(prev);
+	    prev = G;
+	    agwrite(G, outFile);
+	    fflush(outFile);
+	}
+    }
+    exit(rv);
+}
