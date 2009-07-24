6f41d6bec42ffdf6eaa469eef94fbc6ddd5b8507
erg <devnull@localhost>
2009-07-24 18:10:38 +0000

    Add copyright notice

--- cmd/smyrna/glmotion.h
+++ cmd/smyrna/glmotion.h
@@ -1,6 +1,23 @@
+/* vim:set shiftwidth=4 ts=8: */
+
+/**********************************************************
+*      This software is part of the graphviz package      *
+*                http://www.graphviz.org/                 *
+*                                                         *
+*            Copyright (c) 1994-2004 AT&T Corp.           *
+*                and is licensed under the                *
+*            Common Public License, Version 1.0           *
+*                      by AT&T Corp.                      *
+*                                                         *
+*        Information and Software Systems Research        *
+*              AT&T Research, Florham Park NJ             *
+**********************************************************/
+
 #ifndef GLMOTION_H
 #define GLMOTION_H
+
 #include "viewport.h"
+
 void glmotion_main(ViewInfo * v,GdkEventMotion * event,GtkWidget * widget);
 void glmotion_zoom(ViewInfo * v);
 void glmotion_pan(ViewInfo * v);
@@ -8,4 +25,5 @@ void glmotion_rotate(ViewInfo * v);
 void glmotion_adjust_pan(ViewInfo* v,float panx,float pany);
 void graph_zoom(float real_zoom);
 void glmotion_zoom_inc(int zoomin);
+
 #endif
