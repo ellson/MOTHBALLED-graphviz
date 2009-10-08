3395c782f88bf4e8921c7eaa492fe5e9d9b13b1a
arif <devnull@localhost>
2009-10-08 19:50:01 +0000

    *** empty log message ***

---
+++ lib/glcomp/glcompmouse.h
@@ -0,0 +1,31 @@
+/**********************************************************
+*      This software is part of the graphviz package      *
+*                http://www.graphviz.org/                 *
+*                                                         *
+*            Copyright (c) 1994-2007 AT&T Corp.           *
+*                and is licensed under the                *
+*            Common Public License, Version 1.0           *
+*                      by AT&T Corp.                      *
+*                                                         *
+*        Information and Software Systems Research        *
+*              AT&T Research, Florham Park NJ             *
+**********************************************************/
+#ifndef GLCOMPMOUSE_H
+#define GLCOMPMOUSE_H
+
+#include "glcompdefs.h"
+
+
+
+/*events*/
+extern void glCompMouseInit(glCompMouse * m);
+extern void glCompClick(glCompObj * o,GLfloat x,GLfloat y,glMouseButtonType t);
+extern void glCompDoubleClick(glCompObj * obj,GLfloat x,GLfloat y,glMouseButtonType t);
+extern void glCompMouseDown(glCompObj * obj,GLfloat x,GLfloat y,glMouseButtonType t);
+extern void glCompMouseIn(glCompObj * obj,GLfloat x,GLfloat y);
+extern void glCompMouseOut(glCompObj * obj,GLfloat x,GLfloat y);
+extern void glCompMouseOver(glCompObj * obj,GLfloat x,GLfloat y);
+extern void glCompMouseUp(glCompObj * obj,GLfloat x,GLfloat y,glMouseButtonType t);
+extern void glCompMouseDrag(glCompObj * obj,GLfloat dx,GLfloat dy,glMouseButtonType t);
+
+#endif
