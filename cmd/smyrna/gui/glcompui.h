61fc6ac151e7b6f6af46e655de7368c6e1f37fae
arif <devnull@localhost>
2009-10-08 20:14:51 +0000

    *** empty log message ***

---
+++ cmd/smyrna/gui/glcompui.h
@@ -0,0 +1,21 @@
+/* $Id$Revision: */
+/* vim:set shiftwidth=4 ts=8: */
+
+/**********************************************************
+*      This software is part of the graphviz package      *
+*                http://www.graphviz.org/                 *
+*                                                         *
+*            Copyright (c) 1994-2004 AT&T Corp.           *
+*                and is licensed under the                *
+*            Common Public License, Version 1.0           *
+*                      by AT&T Corp.                      *
+*                                                         *
+*        Information and Software Systems Research        *
+*              AT&T Research, Florham Park NJ             *
+**********************************************************/
+
+#ifndef GLCOMPUI_H
+#define GLCOMPUI_H
+#include "smyrnadefs.h"
+extern glCompSet *glcreate_gl_topview_menu(void);
+#endif
