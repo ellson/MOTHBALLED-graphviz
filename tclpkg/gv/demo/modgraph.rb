50465581305b94059fa5f90707e7660360bedd25
ellson <devnull@localhost>
2009-10-14 17:11:21 +0000

    fixes to ruby demo from: Ralf Mueller <ralf.mueller@zmaw.de>

--- tclpkg/gv/demo/modgraph.rb
+++ tclpkg/gv/demo/modgraph.rb
@@ -2,8 +2,9 @@
 
 # display the kernel module dependencies
 
-#author: Patricio Ros <patricioros.dev@gmail.com>
-# based on: modgraph.tcl by  John Ellson <ellson@research.att.com>
+#based on: modgraph.tcl by:  John Ellson <ellson@research.att.com>
+#ruby adaptation by:         Patricio Ros <patricioros.dev@gmail.com>
+#additional fixes:           Ralph Mueller <ralf.mueller@zmaw.de>
 
 require 'gv'
 
@@ -21,14 +22,15 @@ Gv.setv(N, 'fontsize', '8')
 Gv.setv(N, 'fontname', 'helvetica')
 Gv.setv(E, 'arrowsize', '.4')
 
-File.readlines("/proc/modules").each do |rec|
-  fields = rec.split(' ') 
-  n = Gv.node(G, fields[0])
-  fields[3].split(',').find_all {|i| not ['-',' '].include? i}.each do |usedby|
-     Gv.edge(n, Gv.node(G, usedby))
-  end
-end
+modules = File.open('/proc/modules', mode="r").readlines
+
+modules.each {|rec|
+   fields = rec.split(' ')
+   n = Gv.node(G, fields[0])
+   fields[3].split(',').each {|usedby|
+      Gv.edge(n, Gv.node(G, usedby)) if (usedby != '-') and (usedby != '')
+   }
+}
 
 Gv.layout(G, 'dot')
 Gv.render(G, 'xlib')
-
