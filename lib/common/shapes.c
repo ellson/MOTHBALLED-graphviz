27ec21a9adfe0e57565aa19373d274d9c185443a
erg <devnull@localhost>
2010-04-12 21:58:26 +0000

    Fix bug 1923

--- lib/common/shapes.c
+++ lib/common/shapes.c
@@ -2636,6 +2636,8 @@ port resolvePort(node_t * n, node_t * other, port * oldport)
     port rv;
     char *compass = closestSide(n, other, oldport);
 
+    /* transfer name pointer; all other necessary fields will be regenerated */
+    rv.name = oldport->name;
     compassPort(n, oldport->bp, &rv, compass, oldport->side, NULL);
 
     return rv;
