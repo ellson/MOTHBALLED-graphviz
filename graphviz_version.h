36e776be2cda94b009299dce3b0b6e1046decbec
erg <devnull@localhost>
2010-07-22 18:32:50 +0000

    Restore stub contents

--- graphviz_version.h
+++ graphviz_version.h
@@ -1,10 +1,1 @@
-#define GVPLUGIN_CONFIG_FILE "config6"
-#define GVPLUGIN_VERSION 6
-#define PACKAGE "graphviz"
-#define PACKAGE_BUGREPORT "http://www.graphviz.org/"
-#define PACKAGE_NAME "graphviz"
-#define PACKAGE_STRING "graphviz 2.27.20100504.0445"
-#define PACKAGE_TARNAME "graphviz"
-#define PACKAGE_URL ""
-#define PACKAGE_VERSION "2.27.20100504.0445"
-#define VERSION "2.27.20100504.0445"
+/* This is a stub - the real one is generated during packaging */
