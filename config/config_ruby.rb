1748259a971a9a6f7498ca8fb72e7e28e0dfb636
ellson <devnull@localhost>
2011-02-18 21:25:56 +0000

    back out previous change - didn't work for me

--- config/config_ruby.rb
+++ config/config_ruby.rb
@@ -5,7 +5,7 @@ CONFIG = Config::MAKEFILE_CONFIG
 
 case ARGV[0]
 when "INCLUDES"
-    puts "#{Config::expand(CONFIG['rubyhdrdir'])} -I#{Config::expand(CONFIG['rubyhdrdir'])}/#{Config::expand(CONFIG['arch'])}"
+    puts Config::expand(CONFIG["archdir"])
 when "lib"
     puts Config::expand(CONFIG["libdir"])
 when "archsitelib"
