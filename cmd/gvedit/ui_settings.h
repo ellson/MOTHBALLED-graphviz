973f69ba92e64bd4c3301951447ed6d798b96136
arif <devnull@localhost>
2011-02-21 19:56:48 +0000

    *** empty log message ***

--- cmd/gvedit/ui_settings.h
+++ cmd/gvedit/ui_settings.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'settings.ui'
 **
-** Created: Mon Feb 21 14:39:01 2011
+** Created: Mon Feb 21 14:54:23 2011
 **      by: Qt User Interface Compiler version 4.7.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
