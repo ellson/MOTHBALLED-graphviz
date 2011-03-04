61d4b853f1972cf19e6202ca7110068828a54b54
erg <devnull@localhost>
2011-03-04 17:38:16 +0000

    Fix double call to functions when used in MAX macro

--- cmd/dot/dot.c
+++ cmd/dot/dot.c
@@ -173,6 +173,12 @@ static graph_t *create_test_graph(void)
     return g;
 }
 
+static int max (int a, int b)
+{
+    if (a > b) return a;
+    else return b;
+}
+
 int main(int argc, char **argv)
 {
     graph_t *prev = NULL;
@@ -218,10 +224,11 @@ int main(int argc, char **argv)
 	    }
 	    gvLayoutJobs(Gvc, G);  /* take layout engine from command line */
 	    gvRenderJobs(Gvc, G);
-	    rc = MAX(rc,agreseterrors());
+	    rc = max(rc,agreseterrors());
 	    prev = G;
 	}
     }
     gvFinalize(Gvc);
-    return (MAX(rc,gvFreeContext(Gvc)));
+    
+    return (max(rc,gvFreeContext(Gvc)));
 }
