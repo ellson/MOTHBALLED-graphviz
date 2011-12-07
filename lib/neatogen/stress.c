cfc3090a97065233419d6389b94bde1bca178206
Emden Gansner <erg@research.att.com>
2011-12-07 15:18:54 -0500

    Remove some debug code for cgraph version

--- lib/neatogen/stress.c
+++ lib/neatogen/stress.c
@@ -1274,19 +1274,10 @@ int stress_majorization_kD_mkernel(vtx_data * graph,	/* Input graph in sparse re
 		compute_stressf(coords, lap2, dim, n, exp),
 		iterations, elapsed_sec());
     }
-#ifdef WITH_CGRAPH
-    if (Verbose)
-	fprintf(stderr, "coords\n");
-#endif				/* WITH_CGRAPH */
 
     for (i = 0; i < dim; i++) {
 	for (j = 0; j < n; j++) {
 	    d_coords[i][j] = coords[i][j];
-#ifdef WITH_CGRAPH
-	    if (Verbose)
-		fprintf(stderr, "%f\n", coords[i][j]);
-
-#endif				/* WITH_CGRAPH */
 	}
     }
 #ifdef NONCORE
