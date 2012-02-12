4c93c79f69be60bf9dbcc5f165191b74e0967ea0
John Ellson <ellson@research.att.com>
2012-02-12 11:51:13 -0500

    fix more ruby breakage

--- config/config_ruby.rb
+++ config/config_ruby.rb
@@ -10,6 +10,8 @@ when "lib"
     puts Config::expand(CONFIG["libdir"])
 when "vendorarchdir"
     puts Config::expand(CONFIG["vendorarchdir"])
+when "sitearchdir"
+    puts Config::expand(CONFIG["sitearchdir"])
 when "sitelib"
     puts Config::expand(CONFIG["sitedir"])
 end
