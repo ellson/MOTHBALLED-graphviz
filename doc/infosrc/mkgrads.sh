23d8062fd368db46cc17c9695d8d283182f620eb
Emden R. Gansner <erg@research.att.com>
2013-06-28 17:52:20 -0400

    Add documentation on new linear gradient fill type.

--- doc/infosrc/mkgrads.sh
+++ doc/infosrc/mkgrads.sh
@@ -2,7 +2,7 @@
 #
 # usage: mkgrads.sh <gradient>
 #
-# where gradient has the form g_lin<angle>.fmt or g_rad<angle>.fmt
+# where gradient has the form g_lin<angle>.fmt, g_wlin<angle>.fmt or g_rad<angle>.fmt
 # where .fmt gives the desired output format.
 
 if (( $# == 0 ))
@@ -15,11 +15,17 @@ TGT=$1               # g_type.fmt
 GRAD=${1%.*}         # g_type
 FMT=${1#$GRAD.}      # fmt
 F=$GRAD.dot          # g_type.dot
+WT=
 
 case "$GRAD" in
  g_lin* )
   ANG=${GRAD#g_lin}
   STYLE=
+  ;;
+ g_wlin* )
+  ANG=${GRAD#g_wlin}
+  STYLE=
+  WT=';0.3'
   ;;
  g_rad* )
   ANG=${GRAD#g_rad}
@@ -30,7 +36,7 @@ esac
     exec 3> $F
 
     echo "digraph G {" >&3
-    echo "  node[style=filled fillcolor=\"yellow:blue\" shape=box label=\"\"]" >&3
+    echo "  node[style=filled fillcolor=\"yellow${WT}:blue\" shape=box label=\"\"]" >&3
     echo "  H " >&3
     echo "}" >&3
 
