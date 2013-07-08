a01af71449af5b699476117faadca3526f0f9ea6
Emden R. Gansner <erg@research.att.com>
2013-07-08 11:42:30 -0400

    Use RbConfig instead of obsolete and deprecated Config (7, 17 May 2013)

--- config/config_ruby.rb
+++ config/config_ruby.rb
@@ -1,18 +1,18 @@
 require 'rbconfig'
 
-CONFIG = Config::MAKEFILE_CONFIG
+CONFIG = RbConfig::MAKEFILE_CONFIG
 
 
 case ARGV[0]
 when "archdir"
-    puts Config::expand(CONFIG["archdir"])
+    puts RbConfig::expand(CONFIG["archdir"])
 when "lib"
-    puts Config::expand(CONFIG["libdir"])
+    puts RbConfig::expand(CONFIG["libdir"])
 when "vendorarchdir"
-    puts Config::expand(CONFIG["vendorarchdir"])
+    puts RbConfig::expand(CONFIG["vendorarchdir"])
 when "sitearchdir"
-    puts Config::expand(CONFIG["sitearchdir"])
+    puts RbConfig::expand(CONFIG["sitearchdir"])
 when "sitelib"
-    puts Config::expand(CONFIG["sitedir"])
+    puts RbConfig::expand(CONFIG["sitedir"])
 end
 
