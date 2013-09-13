26561d25d67c12a21fdb7a791ffb3b9f21b691e7
Emden R. Gansner <erg@research.att.com>
2013-09-13 10:45:12 -0400

    Remove aspect attribute until the implementation problems can be fixed

--- lib/dotgen/aspect.c
+++ lib/dotgen/aspect.c
@@ -1988,6 +1988,11 @@ setAspect (Agraph_t * g, aspect_t* adata)
 	adata->badGraph = 0;
 	return NULL;
     }
+    agerr (AGWARN, "the aspect attribute has been disabled due to implementation flaws - attribute ignored.\n");
+    
+    adata->nextIter = 0;
+    adata->badGraph = 0;
+    return NULL;
     
     if (rv < MIN_AR) rv = MIN_AR;
     else if (rv > MAX_AR) rv = MAX_AR;
