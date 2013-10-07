e430b2f859536a6ec5d3351c4c378080e323d047
John Ellson <ellson@research.att.com>
2013-10-07 17:19:58 -0400

    fixing older mechanism to work as well until conversion complete

--- set_dev_version.sh
+++ set_dev_version.sh
@@ -5,7 +5,7 @@
 # DO NOT COMMIT TO CVS AFTER RUNNING THIS SCRIPT
 
 # This script is intended for use only during the preparation of master sources for nightly snapshot builds.
-# It modifies configure.ac to use an extended package version number containing the current date.
+# It modifies autogen.sh to use an extended package version number containing the current date.
 #
 # This style of version number is particular to graphviz, and probably wouldn't be done this way if
 # we were using SVN rather than CVS.  Also, it has obscure complications when we try to extend it to the Windows platform.
@@ -24,21 +24,21 @@
 
 DATE=`date -u +%Y%m%d.%H%M`
 
-VERSION_MAJOR=`grep 'm4_define(graphviz_version_major' configure.ac | sed 's/.*, \([0-9]*\))/\1/'`
-VERSION_MINOR=`grep 'm4_define(graphviz_version_minor' configure.ac | sed 's/.*, \([0-9]*\))/\1/'`
-VERSION_MICRO=`grep 'm4_define(graphviz_version_micro' configure.ac | sed 's/.*, \([0-9.]*\))/\1/'`
+VERSION_MAJOR=`grep 'm4_define(graphviz_version_major' autogen.sh | sed 's/.*, \([0-9]*\))/\1/'`
+VERSION_MINOR=`grep 'm4_define(graphviz_version_minor' autogen.sh | sed 's/.*, \([0-9]*\))/\1/'`
+VERSION_MICRO=`grep 'm4_define(graphviz_version_micro' autogen.sh | sed 's/.*, \([0-9.]*\))/\1/'`
 
 VERSION_MICRO=$DATE
-sed "s/\(m4_define(graphviz_version_micro, \)[0-9.]*)/\1$VERSION_MICRO)/" <configure.ac >t$$
-mv t$$ configure.ac
-sed "s/\(GRAPHVIZ_COLLECTION\)=.*/\1=development/" <configure.ac >t$$
-mv t$$ configure.ac
+sed "s/\(m4_define(graphviz_version_micro, \)[0-9.]*)/\1$VERSION_MICRO)/" <autogen.sh >t$$
+mv t$$ autogen.sh
+sed "s/\(GRAPHVIZ_COLLECTION\)=.*/\1=development/" <autogen.sh >t$$
+mv t$$ autogen.sh
 
 VERSION=$VERSION_MAJOR.$VERSION_MINOR.$VERSION_MICRO
 
 VERSION_DATE=$DATE
-sed "s/VERSION_DATE=.*/VERSION_DATE=$VERSION_DATE/" <configure.ac >t$$
-mv t$$ configure.ac
+sed "s/VERSION_DATE=.*/VERSION_DATE=$VERSION_DATE/" <autogen.sh >t$$
+mv t$$ autogen.sh
 
 
 
