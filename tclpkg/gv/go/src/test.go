1b0ecffea4aa533de762bf90787e52ad5c90bd34
John Ellson <ellson@research.att.com>
2013-11-20 17:32:36 -0500

    more go bits

--- tclpkg/gv/go/src/test.go
+++ tclpkg/gv/go/src/test.go
@@ -1,11 +1,12 @@
 package main
-import (
-   "fmt"
-   "example"
-   "gv"
-)
-
+import ( "gv" )
 func main() {
-    fmt.Printf("Hello, world.  fact(2) = %v\n", example.Fact(2))
-    G = gv.Digraph()
+    var g    gv.Agraph_t
+    var n, m gv.Agnode_t
+//  var e    gv.Agedge_t
+    g = gv.Digraph("G")
+    n = gv.Node(g, "hello")
+    m = gv.Node(g, "world")
+        gv.Edge(n, m)
+    gv.Write(g, "hello.out")
 }
