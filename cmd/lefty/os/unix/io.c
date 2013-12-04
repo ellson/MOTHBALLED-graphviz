682d3356245c987bf0981917208f883220c4c213
Emden R. Gansner <erg@alum.mit.edu>
2013-12-04 17:59:19 -0500

    Replace AI_PASSIVE flag with AI_ADDRCONFIG | AI_NUMERICSERV

--- cmd/lefty/os/unix/io.c
+++ cmd/lefty/os/unix/io.c
@@ -293,7 +293,7 @@ static FILE *serverconnect (char *name) {
 
     hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
     hints.ai_socktype = SOCK_STREAM;/* Stream socket */
-    hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
+    hints.ai_flags = AI_ADDRCONFIG | AI_NUMERICSERV;
     hints.ai_protocol = 0;          /* Any protocol */
     hints.ai_canonname = NULL;
     hints.ai_addr = NULL;
