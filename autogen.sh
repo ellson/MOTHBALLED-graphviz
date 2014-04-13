f54ac2c9313ae80ccf76ef4ac6aa9be820a23126
John Ellson <john.ellson@comcast.net>
2014-04-13 16:40:25 -0400

    2.38 stable release

--- autogen.sh
+++ autogen.sh
@@ -11,7 +11,7 @@ dnl                       even => stable series
 dnl For the micro number: 0 => in-progress development
 dnl                       timestamp => tar-file snapshot or release
 m4_define(graphviz_version_major, 2)
-m4_define(graphviz_version_minor, 37)
+m4_define(graphviz_version_minor, 38)
 dnl NB: the next line gets changed to a date/time string for development releases
 m4_define(graphviz_version_micro, 0)
 m4_define(graphviz_version_date, $GRAPHVIZ_VERSION_DATE)
