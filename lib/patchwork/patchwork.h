db9fc2cfdfac82c1584bf0afd0dc7f91d0c30cd0
Erwin Janssen <erwinjanssen@outlook.com>
2016-06-21 14:36:30 +0200

    Revert "Windows: patchwork and xdot now generating .lib files."
    This reverts commit 7d05d9058ace90e06beec60f1aa5b83bb809a109.

--- lib/patchwork/patchwork.h
+++ lib/patchwork/patchwork.h
@@ -17,12 +17,6 @@
 #include "render.h"
 #include "fdp.h"
 
-/*visual studio*/
-#if defined(WIN32)
-#   define extern __declspec(dllexport)
-#endif
-/*end visual studio*/
-
 #ifdef __cplusplus
 extern "C" {
 #endif
