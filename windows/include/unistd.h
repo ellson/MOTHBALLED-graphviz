9a4527b1b7b24ef5431824d1fc08ce409762fad6
Erwin Janssen <erwinjanssen@outlook.com>
2016-10-07 12:35:51 +0200

    Expanded windows/include/unistd.h
    This unistd.h header file is intended to serve as a drop-in replacement
    for unistd.h on Windows.
    Source: http://stackoverflow.com/questions/341817/is-there-a-replacement-for-unistd-h-for-windows-visual-c

--- windows/include/unistd.h
+++ windows/include/unistd.h
@@ -1,12 +1,50 @@
-#ifndef UNISTD_H
-#define UNISTD_H
+#ifndef _UNISTD_H
+#define _UNISTD_H    1
 
-// io.h is the Windows equivalant of unistd.h
+/* This file intended to serve as a drop-in replacement for 
+ * unistd.h on Windows
+ */
+
+#include <stdlib.h>
 #include <io.h>
+#include <process.h> /* for getpid() and the exec..() family */
+#include <direct.h> /* for _getcwd() and _chdir() */
 
-#define	F_OK	0
-#define	R_OK	4
-#define	W_OK	2
-#define	X_OK	1
+#define srandom srand
+#define random rand
 
-#endif 
+/* Values for the second argument to access.
+   These may be OR'd together.  */
+#define R_OK    4       /* Test for read permission.  */
+#define W_OK    2       /* Test for write permission.  */
+//#define   X_OK    1   /* execute permission - unsupported in windows*/
+#define F_OK    0       /* Test for existence.  */
+
+#define access _access
+#define dup2 _dup2
+#define execve _execve
+#define ftruncate _chsize
+#define unlink _unlink
+#define fileno _fileno
+#define getcwd _getcwd
+#define chdir _chdir
+#define isatty _isatty
+#define lseek _lseek
+/* read, write, and close are NOT being #defined here, because while there are file handle specific versions for Windows, they probably don't work for sockets. You need to look at your app and consider whether to call e.g. closesocket(). */
+
+#define ssize_t int
+
+#define STDIN_FILENO 0
+#define STDOUT_FILENO 1
+#define STDERR_FILENO 2
+/* should be in some equivalent to <sys/types.h> */
+typedef __int8            int8_t;
+typedef __int16           int16_t; 
+typedef __int32           int32_t;
+typedef __int64           int64_t;
+typedef unsigned __int8   uint8_t;
+typedef unsigned __int16  uint16_t;
+typedef unsigned __int32  uint32_t;
+typedef unsigned __int64  uint64_t;
+
+#endif /* unistd.h  */
