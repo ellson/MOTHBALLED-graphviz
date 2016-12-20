8e12ecfc62f01ae7012556e1df18ac1ccfb4354a
John Ellson <ellson@research.att.com>
2016-12-20 11:48:14 -0500

    starting 2.41 development series

--- autogen.sh
+++ autogen.sh
@@ -24,7 +24,7 @@ dnl                       even => stable series
 dnl For the micro number: 0 => in-progress development
 dnl                       timestamp => tar-file snapshot or release
 m4_define(graphviz_version_major, 2)
-m4_define(graphviz_version_minor, 40)
+m4_define(graphviz_version_minor, 41)
 dnl NB: the next line gets changed to a date/time string for development releases
 m4_define(graphviz_version_micro, $GRAPHVIZ_VERSION_DATE)
 m4_define(graphviz_version_date, $GRAPHVIZ_VERSION_DATE)
