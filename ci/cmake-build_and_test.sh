74afcb5194e3e4d182f5564d40ea6abf8c3c00b8
Erwin Janssen <erwinjanssen@outlook.com>
2016-12-29 12:41:06 +0100

    Add install target and initial packaging to CMake
    The installation locations are identical to the Autotools install step,
    but the pdf version of the manpages aren't generated yet.
    With CPack, packages can be generated based on the `install` commands. For
    now only the basic ZIP package generator is added.
    Added the new CMake commands to the Travis and Appveyor scripts.

--- ci/cmake-build_and_test.sh
+++ ci/cmake-build_and_test.sh
@@ -19,3 +19,17 @@ else
     echo "Error: 'cmake --build .' failed." >&2
     exit 1
 fi
+
+if cpack ; then
+    echo "'cpack' succesfull."
+else
+    echo "Error: 'cpack' failed." >&2
+    exit 1
+fi
+
+if make install ; then
+    echo "'make install' succesfull."
+else
+    echo "Error: 'make install' failed." >&2
+    exit 1
+fi
