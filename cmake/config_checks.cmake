f13cfd8c9d5570b8a73fd146b6efc38fb46e7b36
Erwin Janssen <erwinjanssen@outlook.com>
2017-01-03 18:01:50 +0100

    Add lib/pathplan to the CMake build
    Only a one additional checks were required for pathplan: cbrt function

--- cmake/config_checks.cmake
+++ cmake/config_checks.cmake
@@ -6,7 +6,8 @@ CHECK_INCLUDE_FILE(unistd.h HAVE_UNISTD_H)
 # Function checks
 include(CheckFunctionExists)
 
-CHECK_FUNCTION_EXISTS(strcasecmp HAVE_STRCASECMP)
+CHECK_FUNCTION_EXISTS( cbrt        HAVE_CBRT       )
+CHECK_FUNCTION_EXISTS( strcasecmp  HAVE_STRCASECMP )
 
 # Write check results to config.h header
 configure_file(config-cmake.h.in config.h)
