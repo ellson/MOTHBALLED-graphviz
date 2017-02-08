3e0dec7705784cd454d9a558d2685386b82d16f4
Erwin Janssen <erwinjanssen@outlook.com>
2017-02-08 18:14:10 +0100

    Remove `AC_HEADER_TIME` from configure.ac
    This check is deprecated according to the official autoconf documentation,
    it can safely be removed.

--- windows/include/config.h
+++ windows/include/config.h
@@ -339,9 +339,6 @@
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
-/* Define to 1 if you can safely include both <sys/time.h> and <time.h>. */
-#define TIME_WITH_SYS_TIME 1
-
 /* Define if you want CGRAPH */
 #define WITH_CGRAPH 1
 
