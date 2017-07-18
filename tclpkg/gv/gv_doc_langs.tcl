32466ecc452102a1fc84f4331c0ed099da4e7edc
John Ellson <ellson@research.att.com>
2017-07-18 16:26:11 -0400

    python -> python2 and python3 (incomplete)

--- tclpkg/gv/gv_doc_langs.tcl
+++ tclpkg/gv/gv_doc_langs.tcl
@@ -401,6 +401,98 @@ array set LANGS {
 		USAGE {
 		}
 	}
+	python2 {
+		TYPES {
+			{Agraph_t* g} graph_handle
+			{Agraph_t* sg} subgraph_handle
+			{Agnode_t* n} node_handle
+			{Agnode_t* t} tail_node_handle
+			{Agnode_t* h} head_node_handle
+			{Agedge_t* e} edge_handle
+			{Agsym_t* a} attr_handle
+			{char* gne} type
+			{char* name} name
+			{char* tname} tail_name
+			{char* hname} head_name
+			{char* attr} attr_name
+			{char* val} attr_value
+			{const char* filename} filename
+			{char* engine} engine
+			{char* string} string
+			{char** outdata} outdata
+			{char* format} format
+			{FILE* f} channel
+			{void** data} data_handle
+			Agraph_t* graph_handle
+			Agnode_t* node_handle
+			Agedge_t* edge_handle
+			Agsym_t* attribute_handle
+			char* string
+			{const char*} string
+			char** outdata
+			FILE* channel
+			bool bool
+			int int
+			void** data_handle
+			void {}
+		}
+		SYNTAX {
+			gv.  (  {, }   {);}
+		}
+		SYNOPSIS {
+			{#!/usr/bin/python2}
+			{import sys}
+			{import gv}
+		}
+		USAGE {
+		}
+	}
+	python3 {
+		TYPES {
+			{Agraph_t* g} graph_handle
+			{Agraph_t* sg} subgraph_handle
+			{Agnode_t* n} node_handle
+			{Agnode_t* t} tail_node_handle
+			{Agnode_t* h} head_node_handle
+			{Agedge_t* e} edge_handle
+			{Agsym_t* a} attr_handle
+			{char* gne} type
+			{char* name} name
+			{char* tname} tail_name
+			{char* hname} head_name
+			{char* attr} attr_name
+			{char* val} attr_value
+			{const char* filename} filename
+			{char* engine} engine
+			{char* string} string
+			{char** outdata} outdata
+			{char* format} format
+			{FILE* f} channel
+			{void** data} data_handle
+			Agraph_t* graph_handle
+			Agnode_t* node_handle
+			Agedge_t* edge_handle
+			Agsym_t* attribute_handle
+			char* string
+			{const char*} string
+			char** outdata
+			FILE* channel
+			bool bool
+			int int
+			void** data_handle
+			void {}
+		}
+		SYNTAX {
+			gv.  (  {, }   {);}
+		}
+		SYNOPSIS {
+			{#!/usr/bin/python3}
+			{import sys}
+			{import gv}
+		}
+		USAGE {
+		}
+	}
 	R {
 		TYPES {
 			Agraph_t* SWIGTYPE_p_Agraph_t
